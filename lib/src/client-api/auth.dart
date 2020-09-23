dbb2f5a1566f49450266dd1d85027a5dd6fce9bc
Alan Ung <alanu@student.unimelb.edu.au>
2020-09-24 08:08:52 +1000

    Style: "..." -> '...'

--- lib/src/client-api/auth.dart
+++ lib/src/client-api/auth.dart
@@ -19,7 +19,7 @@ class AuthModel {
 
   /// Constructor for internal use only
   AuthModel(this._keyValueStore) {
-    _token = _keyValueStore.getString("token");
+    _token = _keyValueStore.getString('token');
   }
 
   bool inSession() {
@@ -28,12 +28,12 @@ class AuthModel {
 
   Future<bool> join() async {
     final http.Response res =
-        await http.post("$AUTH_URL/join", headers: ApiBase.headers());
+        await http.post('$AUTH_URL/join', headers: ApiBase.headers());
 
     if (res.statusCode == 200) {
       String token = json.decode(res.body)['token'];
       this._token = token;
-      _keyValueStore.setString("token", token);
+      _keyValueStore.setString('token', token);
       return true;
     } else {
       return null;
@@ -42,7 +42,7 @@ class AuthModel {
 
   Future<RegisteredUser> register(
       String email, String password, String name) async {
-    final http.Response res = await http.post("$AUTH_URL/register",
+    final http.Response res = await http.post('$AUTH_URL/register',
         headers: ApiBase.headers(),
         body: jsonEncode(<String, String>{
           'email': email,
@@ -59,14 +59,14 @@ class AuthModel {
   }
 
   Future<bool> login(String email, String password) async {
-    final http.Response res = await http.post("$AUTH_URL/login",
+    final http.Response res = await http.post('$AUTH_URL/login',
         headers: ApiBase.headers(),
         body:
             jsonEncode(<String, String>{'email': email, 'password': password}));
 
     if (res.statusCode == 200) {
       String token = json.decode(res.body)['token'];
-      _keyValueStore.setString("token", token);
+      _keyValueStore.setString('token', token);
       return true;
     } else {
       // todo
@@ -76,12 +76,12 @@ class AuthModel {
 
   Future<bool> sessionIsValid() async {
     final http.Response res =
-        await http.get("/session", headers: ApiBase.headers(authToken: token));
+        await http.get('/session', headers: ApiBase.headers(authToken: token));
     return res.statusCode == 200;
   }
 
   Future<bool> logout() async {
-    final http.Response res = await http.post(AUTH_URL + "/logout",
+    final http.Response res = await http.post(AUTH_URL + '/logout',
         headers: ApiBase.headers(authToken: token));
     if (res.statusCode == 200) {
       _token = '';
