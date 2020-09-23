b0bdf2fa4a737f93411130b6e35db821e7649500
Alan Ung <alanu@student.unimelb.edu.au>
2020-09-23 18:25:23 +1000

    ApiBase: add documentation

--- lib/client-api/api_base.dart
+++ lib/client-api/api_base.dart
@@ -1,6 +1,23 @@
+/// Class providing the base URL and headers for requests to the backend server
+/// This class is not for direct use by the UI.
 class ApiBase {
   static const String BASE_URL = 'https://fuzzybroccoli.com';
 
+  /// Return headers used for HTTP requests to the backend server.
+  /// Calling `headers()` returns the default headers:
+  ///   ```
+  ///   {
+  ///     'Content-Type': 'application/json; charset=UTF-8'
+  ///   }
+  ///   ```
+  /// Content-Type and Authorization (none by default) can be specified.
+  /// For example, calling `headers(authToken: '123456')` returns:
+  ///   ```
+  ///   {
+  ///     'Content-Type': 'application/json; charset=UTF-8',
+  ///     'Authorization': 'Bearer 123456'
+  ///   }
+  ///   ```
   static Map<String, String> headers(
       {String contentType = 'application/json; charset=UTF-8',
       String authToken}) {
