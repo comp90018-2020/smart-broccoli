90a78197a643482f6d29dbf3362bd1faaa6d092a
Alan Ung <alanu@student.unimelb.edu.au>
2020-09-23 18:11:02 +1000

    ApiBase.headers: extract Content-Type into named default param

--- lib/client-api/api_base.dart
+++ lib/client-api/api_base.dart
@@ -1,14 +1,16 @@
 class ApiBase {
   static const String BASE_URL = 'https://fuzzybroccoli.com';
 
-  static Map<String, String> headers({String authToken}) {
+  static Map<String, String> headers(
+      {String contentType = 'application/json; charset=UTF-8',
+      String authToken}) {
     if (authToken == null)
       return <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
+        'Content-Type': contentType,
       };
 
     return <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
+      'Content-Type': contentType,
       'Authorization': 'Bearer $authToken'
     };
   }
