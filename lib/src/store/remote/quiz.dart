58b91f5bdd0c21aa2cb9671ce12e693fae610587
Alan Ung <alanu@student.unimelb.edu.au>
2020-09-28 17:17:38 +1000

    Add QuizModel.getQuizzes

--- lib/src/store/remote/quiz.dart
+++ lib/src/store/remote/quiz.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:fuzzy_broccoli/models.dart';
+import 'package:http/http.dart' as http;
+
 import 'api_base.dart';
 import 'auth.dart';
 
@@ -10,4 +15,19 @@ class QuizModel {
 
   /// Constructor for external use
   QuizModel(this._authModel);
+
+  /// Return a list of all quizzes created by the authenticated user.
+  Future<List<Quiz>> getQuizzes() async {
+    http.Response response = await http.get(QUIZ_URL,
+        headers: ApiBase.headers(authToken: _authModel.token));
+
+    if (response.statusCode == 200)
+      return (json.decode(response.body) as List)
+          .map((repr) => Quiz.fromJson(repr))
+          .toList();
+
+    if (response.statusCode == 401) throw UnauthorisedRequestException();
+    if (response.statusCode == 403) throw ForbiddenRequestException();
+    throw Exception('Unable to get quizzes: unknown error occurred');
+  }
 }
