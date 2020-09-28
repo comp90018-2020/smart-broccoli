b4947022dd86ff5aa185942ecbe2f4ef786b758c
Qifan Deng <plocircle@live.com>
2020-09-28 20:28:34 +0800

    backup before merge new back-end branch

--- backend/game/index.ts
+++ backend/game/index.ts
@@ -2,6 +2,9 @@ import { json } from 'sequelize';
 import { io } from 'server';
 import { Server, Namespace } from 'socket.io';
 import { Quiz } from './quiz';
+import { jwtVerify } from "../helpers/jwt"
+
+let secret: string = "aaa";
 
 let namespace: Namespace;
 const quiz = new Quiz();
@@ -12,13 +15,88 @@ export default (socketIO: Server) => {
         console.log('connected');
 
         // Emit an event to client
-        socket.emit('message', JSON.stringify({"serverTs": Date.now()}));
+        socket.emit('message', JSON.stringify({ "serverTs": Date.now() }));
 
-        socket.on('joinQuiz', (content: any) => {
-            console.log(socket.handshake);
-
+        // activate quiz
+        socket.on('activateQuiz', (content: any) => {
+            try {
+                jwtVerify(socket.handshake.query.token, secret);
+            }
+            catch (err) {
+                console.error(err);
+            }
         });
 
+        // abort quiz
+        socket.on('abortQuiz', (content: any) => {
+            try {
+                jwtVerify(socket.handshake.query.token, secret);
+            }
+            catch (err) {
+                console.error(err);
+            }
+        });
+
+        // join quiz
+        socket.on('joinQuiz', (content: any) => {
+            try {
+                jwtVerify(socket.handshake.query.token, secret);
+            }
+            catch (err) {
+                console.error(err);
+            }
+        });
+
+        // start quiz
+        socket.on('startQuiz', (content: any) => {
+            try {
+                jwtVerify(socket.handshake.query.token, secret);
+            }
+            catch (err) {
+                console.error(err);
+            }
+        });
+
+        // answer quiz
+        socket.on('answerQuiz', (content: any) => {
+            try {
+                jwtVerify(socket.handshake.query.token, secret);
+            }
+            catch (err) {
+                console.error(err);
+            }
+        });
+
+        // next question
+        socket.on('nextQuestion', (content: any) => {
+            try {
+                jwtVerify(socket.handshake.query.token, secret);
+            }
+            catch (err) {
+                console.error(err);
+            }
+        });
+
+        // show leader board
+        socket.on('showLeaderBoard', (content: any) => {
+            try {
+                jwtVerify(socket.handshake.query.token, secret);
+            }
+            catch (err) {
+                console.error(err);
+            }
+        });
+
+        // end quiz
+        socket.on('endQuiz', (content: any) => {
+            try {
+                jwtVerify(socket.handshake.query.token, secret);
+            }
+            catch (err) {
+                console.error(err);
+            }
+        });
+        
         return next();
     })
 
