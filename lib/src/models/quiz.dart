986f1747b974bffc433a53708f14ca91575c52a9
Alan Ung <alanu@student.unimelb.edu.au>
2020-09-28 17:14:39 +1000

    Add factory methods to quiz-related classes

--- lib/src/models/quiz.dart
+++ lib/src/models/quiz.dart
@@ -11,26 +11,54 @@ class Quiz {
 
   int timeLimit;
   List<Question> questions;
+
+  Quiz(this.id, this.title, this.groupId, this.type,
+      {this.description, this.isActive, this.timeLimit, this.questions});
+
+  factory Quiz.fromJson(Map<String, dynamic> json) =>
+      Quiz(json['id'], json['title'], json['groupId'], json['type'],
+          description: json['description'],
+          isActive: json['isActive'],
+          timeLimit: json['timeLimit']);
 }
 
 abstract class Question {
   int id;
   String text;
   int imgId;
+
+  Question(this.id, this.text, this.imgId);
 }
 
 // True/false question
-class TFQuestion {
+class TFQuestion extends Question {
   bool answer;
+
+  TFQuestion(int id, String text, int imgId, this.answer)
+      : super(id, text, imgId);
+
+  factory TFQuestion.fromJson(Map<String, dynamic> json) =>
+      TFQuestion(json['id'], json['text'], json['imgid'], json['tf']);
 }
 
 // Multiple choice question
-class MCQuestion {
+class MCQuestion extends Question {
   List<QuestionOption> options;
+
+  MCQuestion(int id, String text, int imgId, {this.options})
+      : super(id, text, imgId);
+
+  factory MCQuestion.fromJson(Map<String, dynamic> json) =>
+      MCQuestion(json['id'], json['text'], json['imgid']);
 }
 
 // Option for multiple choice question
 class QuestionOption {
   String text;
   bool correct;
+
+  QuestionOption(this.text, this.correct);
+
+  factory QuestionOption.fromJson(Map<String, dynamic> json) =>
+      QuestionOption(json['text'], json['correct']);
 }
