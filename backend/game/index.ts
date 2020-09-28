4b1737eb88111c38b47bc75030cdb2b697f85d97
Qifan Deng <plocircle@live.com>
2020-09-28 22:11:08 +0800

    WIP: load quiz to session

--- backend/game/index.ts
+++ backend/game/index.ts
@@ -1,13 +1,11 @@
-import { json } from 'sequelize';
-import { io } from 'server';
+import { any } from 'sequelize/types/lib/operators';
 import { Server, Namespace } from 'socket.io';
-import { Quiz } from './quiz';
-import { jwtVerify } from "../helpers/jwt"
+import { LiveQuiz } from './quiz';
 
-let secret: string = "aaa";
+
 
 let namespace: Namespace;
-const quiz = new Quiz();
+const handler = new LiveQuiz();
 
 export default (socketIO: Server) => {
     socketIO.use(async (socket, next) => {
@@ -16,87 +14,55 @@ export default (socketIO: Server) => {
 
         // Emit an event to client
         socket.emit('message', JSON.stringify({ "serverTs": Date.now() }));
-
-        // activate quiz
-        socket.on('activateQuiz', (content: any) => {
-            try {
-                jwtVerify(socket.handshake.query.token, secret);
-            }
-            catch (err) {
-                console.error(err);
-            }
-        });
-
-        // abort quiz
-        socket.on('abortQuiz', (content: any) => {
-            try {
-                jwtVerify(socket.handshake.query.token, secret);
-            }
-            catch (err) {
-                console.error(err);
-            }
-        });
-
-        // join quiz
-        socket.on('joinQuiz', (content: any) => {
-            try {
-                jwtVerify(socket.handshake.query.token, secret);
-            }
-            catch (err) {
-                console.error(err);
-            }
-        });
-
-        // start quiz
-        socket.on('startQuiz', (content: any) => {
-            try {
-                jwtVerify(socket.handshake.query.token, secret);
-            }
-            catch (err) {
-                console.error(err);
-            }
-        });
-
-        // answer quiz
-        socket.on('answerQuiz', (content: any) => {
-            try {
-                jwtVerify(socket.handshake.query.token, secret);
-            }
-            catch (err) {
-                console.error(err);
-            }
-        });
-
-        // next question
-        socket.on('nextQuestion', (content: any) => {
-            try {
-                jwtVerify(socket.handshake.query.token, secret);
-            }
-            catch (err) {
-                console.error(err);
-            }
-        });
-
-        // show leader board
-        socket.on('showLeaderBoard', (content: any) => {
-            try {
-                jwtVerify(socket.handshake.query.token, secret);
-            }
-            catch (err) {
-                console.error(err);
-            }
-        });
-
-        // end quiz
-        socket.on('endQuiz', (content: any) => {
-            try {
-                jwtVerify(socket.handshake.query.token, secret);
-            }
-            catch (err) {
-                console.error(err);
-            }
-        });
         
+        socket.on('test', (msg:string) =>{
+            console.log(msg);
+        });
+        try {
+            // activate quiz
+            socket.on('activateQuiz', (content: any) => {
+                console.log(content);
+                handler.activeQuiz(socket, content);
+            });
+
+            // abort quiz
+            socket.on('abortQuiz', (content: any) => {
+                handler.abortQuiz(socket ,content);
+            });
+
+            // join quiz
+            socket.on('joinQuiz', (content: any) => {
+                handler.joinQuiz(socket, content);
+            });
+
+            // start quiz
+            socket.on('startQuiz', (content: any) => {
+                handler.startQuiz(socket, content);
+            });
+
+            // answer quiz
+            socket.on('answerQuiz', (content: any) => {
+                handler.answerQuiz(socket, content);
+            });
+
+            // next question
+            socket.on('nextQuestion', (content: any) => {
+                handler.nextQuestion(socket, content);
+            });
+
+            // show leader board
+            socket.on('getLeaderBoard', (content: any) => {
+                handler.getLeaderBoardQuiz(socket);
+            });
+
+            // end quiz
+            socket.on('endQuiz', (content: any) => {
+                handler.endQuiz(socket);
+            });
+        }
+        catch (err) {
+            console.error(err);
+        }
         return next();
     })
 
--- backend/game/quiz.ts
+++ backend/game/quiz.ts
@@ -1,38 +1,144 @@
 import { ContextHandlerImpl } from "express-validator/src/chain";
+import { jwtVerify } from "../helpers/jwt";
+class Quiz{
+    quiz : {[key:number] : any};
+    pos = -1;
+    constructor(quizId: number){
+    }
+    loadQuiz(quizId : number){
+        this.quiz[1] = {};
+    }
+    next(){
+        return this.quiz[this.pos++];
+    }
+    
 
-class Quiz {
+}
+class LiveQuiz {
+    // shaerd obj saves live quiz sess
+    sess: {
+        [key: number]: {
+            [key: string]: any
+        }
+    };
+    secret: string = "aaa";
+
 
     constructor() { }
-    
-    isTokenValid(token: string): boolean{
-        return true;
-    }
-
-    isCodeValid(code:string): boolean{
-        return true;
-    }
 
     /**
-     * 
-     * @param content Json string from client
+     *  Verify socket connection using jwt token 
+     * @param socket socket
      */
-    handle(content: any): [boolean, string] {
-        let ret = false;
-        let response:{[key: string]: any} = {'success':false};
-        if (this.isTokenValid(content.token)){
-            if (this.isCodeValid(content.code)){
-                ret = true;
-                response.msg = "You have joined in " + content.quizId;
-                response.success = true;
-            }else{
-                response.err ="Invalid code" 
-            }
+    verifySocketConn(socket: SocketIO.Socket) {
+        // jwtVerify(socket.handshake.query.token, this.secret);
+    }
 
-        }else{
-            response.err ="Invalid token" 
+    loadQuiz(quizId: number){
+        return {}
+    }
+
+    activeQuiz(socket: SocketIO.Socket, content: any) {
+        // verify connection
+        this.verifySocketConn(socket);
+
+        const quizId = content.quizId;
+
+        // json resoonse  
+        let ret: { [key: string]: string };
+        console.log(content);
+        if (quizId in this.sess) {
+            // quiz is in session
+            if (this.sess[quizId].status == "inactive") {
+                // and quiz is inactive
+                this.sess[quizId].status = "active";
+                ret = { "res": "success" }
+            } else {
+                ret = { "res": "failed", "msg": `Quiz ${quizId} is ${this.sess[quizId].status}` };
+            }
+        } else {
+            // quiz is not in session
+            // init quiz in session and make the status be active
+            this.sess[quizId].status = "active";
+            ret = { "res": "success" }
         }
-        return [ret, JSON.stringify(response)];
+
+        // response to the end
+        socket.send(ret);
+    }
+
+    abortQuiz(socket: SocketIO.Socket, content: any) {
+        // verify connection
+        this.verifySocketConn(socket);
+
+        const quizId = content.quizId;
+
+        // json resoonse  
+        const ret = { "res": "success" };
+        this.sess[quizId].status = "active";
+
+        // response to the end
+        socket.send(ret);
+
+    }
+
+    joinQuiz(socket: SocketIO.Socket, content: any) {
+        // verify connection
+        this.verifySocketConn(socket);
+
+        const quizId = content.quizId;
+
+        // json resoonse  
+        let ret: { [key: string]: string };
+        if (quizId in this.sess && this.sess[quizId].status === 'active') {
+            // socket join in a room named ${quizId}
+            socket.join(quizId);
+            ret = { "res": "success" };
+        } else{
+            ret = { "res": "falied", "msg":"`${quizId} is not active: `${this.sess[quizId].status}``" };
+        }
+
+        // response to the end
+        socket.send(ret);
+
+    }
+
+    startQuiz(socket: SocketIO.Socket, content: any) {
+        // verify connection
+        this.verifySocketConn(socket);
+
+        const quizId = content.quizId;
+
+        // json resoonse  
+        let ret: { [key: string]: string };
+        if (quizId in this.sess && this.sess[quizId].status === 'active') {
+            ret = { "res": "success" };
+            // broadcast to the room that quiz has been started
+            socket.to(quizId).send(ret);
+            
+            // broadcast to the room the first question
+            this.nextQuestion(socket, content);
+        } else{
+            ret = { "res": "falied", "msg":"`${quizId} is not active: `${this.sess[quizId].status}``" };
+            socket.send(ret);
+        }
+    }
+
+    answerQuiz(socket: SocketIO.Socket, content: any) {
+
+    }
+
+    nextQuestion(socket: SocketIO.Socket, content: any) {
+
+    }
+
+    getLeaderBoardQuiz(socket: SocketIO.Socket) {
+
+    }
+
+    endQuiz(socket: SocketIO.Socket) {
+
     }
 }
 
-export { Quiz };
+export { LiveQuiz };
