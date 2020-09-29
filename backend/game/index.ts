8b22f118c5c5d295f370f46cdb43a2089244f4cc
Qifan Deng <plocircle@live.com>
2020-09-29 12:07:16 +0800

    WIP: get quiz data from database

--- backend/game/index.ts
+++ backend/game/index.ts
@@ -14,51 +14,56 @@ export default (socketIO: Server) => {
 
         // Emit an event to client
         socket.emit('message', JSON.stringify({ "serverTs": Date.now() }));
-        
-        socket.on('message', (content:any) =>{
-            console.log(typeof(content));
+
+        socket.on('message', (content: any) => {
+            console.log(typeof (content));
             console.log(content);
         });
-            // activate quiz
-            socket.on('activateQuiz', (content: any) => {
-                handler.activeQuiz(socket, content);
-            });
+        // activate quiz
+        socket.on('activateQuiz', (content: any) => {
+            handler.activeQuiz(socket, content);
+        });
 
-            // abort quiz
-            socket.on('abortQuiz', (content: any) => {
-                handler.abortQuiz(socket ,content);
-            });
+        // abort quiz
+        socket.on('abortQuiz', (content: any) => {
+            handler.abortQuiz(socket, content);
+        });
 
-            // join quiz
-            socket.on('joinQuiz', (content: any) => {
-                handler.joinQuiz(socket, content);
-            });
+        // join quiz
+        socket.on('joinQuiz', (content: any) => {
+            handler.joinQuiz(socket, content);
+        });
 
-            // start quiz
-            socket.on('startQuiz', (content: any) => {
-                handler.startQuiz(socket, content);
-            });
+        // start quiz
+        socket.on('startQuiz', (content: any) => {
+            handler.startQuiz(socket, content);
+        });
 
-            // answer quiz
-            socket.on('answerQuiz', (content: any) => {
-                handler.answerQuiz(socket, content);
-            });
+        // answer quiz
+        socket.on('answerQuiz', (content: any) => {
+            handler.answerQuiz(socket, content);
+        });
 
-            // next question
-            socket.on('nextQuestion', (content: any) => {
-                handler.nextQuestion(socket, content);
-            });
+        // next question
+        socket.on('nextQuestion', (content: any) => {
+            handler.nextQuestion(socket, content);
+        });
 
-            // show leader board
-            socket.on('getLeaderBoard', (content: any) => {
-                handler.getLeaderBoardQuiz(socket);
-            });
+        // show leader board
+        socket.on('releaseLeaderBoard', (content: any) => {
+            handler.releaseLeaderBoardQuiz(socket, content);
+        });
 
-            // end quiz
-            socket.on('endQuiz', (content: any) => {
-                handler.endQuiz(socket);
-            });
-    
+        // end quiz
+        socket.on('endQuiz', (content: any) => {
+            handler.endQuiz(socket, content);
+        });
+
+        // get quiz status in case of user drop out during the quiz
+        socket.on('getQuizStatus', (content: any) => {
+            handler.getQuizStatus(socket, content);
+        });
+
         return next();
     })
 
