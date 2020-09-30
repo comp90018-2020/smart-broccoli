d4908838b3a868281cba4cddef059e7c67a17b59
Alan Ung <alanu@student.unimelb.edu.au>
2020-10-01 09:15:34 +1000

    Add test: get participant user profile

--- test/services/user_test.dart
+++ test/services/user_test.dart
@@ -36,4 +36,31 @@ main() async {
     expect(user.email, "foo@bar.com");
     expect(user.name, "Foo Bar");
   });
+
+  test('Get participant user profile', () async {
+    final http.Client client = MockClient();
+    AuthModel am = AuthModel(
+        MainMemKeyValueStore(init: {"token": "asdfqwerty1234567890foobarbaz"}),
+        mocker: client);
+    UserModel um = UserModel(am, mocker: client);
+
+    when(client.get('${UserModel.USER_URL}/profile',
+            headers: anyNamed("headers")))
+        .thenAnswer((_) async => http.Response(
+            json.encode(<String, dynamic>{
+              "id": 1,
+              "email": "foo@bar.com",
+              "name": "Foo Bar",
+              "role": "participant",
+              "pictureId": null,
+              "createdAt": "2020-01-01T00:00:00.000Z",
+              "updatedAt": "2020-01-01T00:00:00.000Z"
+            }),
+            200));
+    final user = await um.getUser();
+    expect(user, isA<ParticipantUser>());
+    expect(user.id, 1);
+    expect(user.email, "foo@bar.com");
+    expect(user.name, "Foo Bar");
+  });
 }
