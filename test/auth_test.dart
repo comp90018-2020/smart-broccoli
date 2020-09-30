ac6ef7107c520bea972511e4371d3f7efeb67261
Alan Ung <alanu@student.unimelb.edu.au>
2020-09-30 21:50:25 +1000

    Add test: register user

---
+++ test/auth_test.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fuzzy_broccoli/cache.dart';
+import 'package:fuzzy_broccoli/models.dart';
+import 'package:fuzzy_broccoli/server.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
+
+class MockClient extends Mock implements http.Client {}
+
+main() async {
+  test('Register user', () async {
+    final http.Client client = MockClient();
+    AuthModel am = AuthModel(MainMemKeyValueStore(), mocker: client);
+
+    when(client.post('${AuthModel.AUTH_URL}/register',
+            headers: anyNamed("headers"), body: anyNamed("body")))
+        .thenAnswer((_) async => http.Response(
+            json.encode(<String, dynamic>{
+              "id": 1,
+              "email": "foo@bar.com",
+              "name": "Foo Bar",
+              "role": "user",
+              "updatedAt": "2020-01-01T00:00:00.000Z",
+              "createdAt": "2020-01-01T00:00:00.000Z",
+              "pictureId": null
+            }),
+            201));
+
+    RegisteredUser user =
+        await am.register("foo@bar.com", "helloworld", "Foo Bar");
+    expect(user.id, 1);
+    expect(user.email, "foo@bar.com");
+    expect(user.name, "Foo Bar");
+  });
+}
