81f859a50952ecb956059d6a4fa0d064f037b3ca
Alan Ung <alanu@student.unimelb.edu.au>
2020-09-30 18:35:11 +1000

    Services: quiz management (#38)
    * Add quiz-related classes
    
    Quiz
    QuizType (enum)
    Question (abstract)
    TFQuestion
    MCQuestion
    QuestionOption
    
    * Add QuizModel stub
    
    * Add factory methods to quiz-related classes
    
    * Add QuizModel.getQuizzes
    
    * Fix Quiz.fromJson factory method
    
    * Remove unused import
    
    * Add QuizModel.getQuiz
    
    * Implement toJson method for quiz-related classes
    
    * Fix Quiz.fromJson when questions = null
    
    * Fix MCQuestion.fromJson
    
    * Minor code formatting
    
    * Add QuizModel.updateQuiz
    
    * Fix serialisation of list fields
    
    * Quiz: separate external and internal constructors
    
    Also add documentation
    
    * Minor change to docs
    
    * TFQuestion, MCQuestion: separate int/ext constructors
    
    * QuestionOption: add documentation
    
    * Quiz: fix questions field not defaulting to []
    
    * Add QuizModel.createQuiz
    
    * QuizModel.updateQuiz: Future<void> -> Future<Quiz>
    
    * MCQuestion: remove misleading [] in options declaration
    
    * QuizModel.getQuizzes: add caveat to docs
    
    When getting all quizzes, questions won't be retrieved.
    
    * Add QuizModel.deleteQuiz
    
    * Add QuizNotFoundException
    
    * Add QuizModel.setQuizPicture, QuizModel.setQuestionPicture
    
    * Add QuizModel.getQuizPicture, QuizModel.getQuestionPicture
    
    * Consistency in docs
    
    * Update documentation
    
    * Change signature of QuizModel.deleteQuiz
    
    * Change signature of QuizModel.getQuizPicture
    
    * Change signature of QuizModel.setQuizPicture
    
    * Fix documentation
    
    * Change signature of QuizModel.getQuestionPicture
    
    * Change signature of QuizModel.setQuestionPicture
    
    * Make Question class hold reference to Quiz
    
    * Remove `quiz` param from QuizModel.{get/set}QuestionPicture
    
    Since Question now hold ref to Quiz.
    
    * Add QuizModel.deleteQuizPicture
    
    * Add QuizModel.deleteQuestionPicture

--- lib/models.dart
+++ lib/models.dart
@@ -1,2 +1,3 @@
 export 'src/models/group.dart';
+export 'src/models/quiz.dart';
 export 'src/models/user.dart';
--- lib/server.dart
+++ lib/server.dart
@@ -1,3 +1,4 @@
 export 'src/store/remote/auth.dart';
 export 'src/store/remote/group.dart';
+export 'src/store/remote/quiz.dart';
 export 'src/store/remote/user.dart';
