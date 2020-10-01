d0874d0f7a6fdf6f50d092491c23493f21cbf50a
Alan Ung <alanu@student.unimelb.edu.au>
2020-10-01 20:44:14 +1000

    Add test: leave group

--- test/services/group_test.dart
+++ test/services/group_test.dart
@@ -454,4 +454,57 @@ main() async {
     expect(() async => await gm.refreshCode(g),
         throwsA(isA<ForbiddenRequestException>()));
   });
+
+  test('Leave group', () async {
+    final http.Client client = MockClient();
+    final AuthModel am = AuthModel(
+        MainMemKeyValueStore(init: {"token": "asdfqwerty1234567890foobarbaz"}),
+        mocker: client);
+    final GroupModel gm = GroupModel(am, mocker: client);
+
+    when(client.post('${GroupModel.GROUP_URL}/2/leave',
+            headers: anyNamed("headers")))
+        .thenAnswer((_) async => http.Response("", 204));
+
+    // pretend client obtained `g` was from `getGroup` or `getGroups`
+    Group g = Group.fromJson(<String, dynamic>{
+      "id": 2,
+      "name": "foo",
+      "createdAt": "2020-01-01T00:00:00.000Z",
+      "updatedAt": "2020-01-01T00:00:00.000Z",
+      "defaultGroup": false,
+      "code": "BG1egA",
+      "role": "owner"
+    });
+
+    await gm.leaveGroup(g);
+    // no exception should be raised
+  });
+
+  test('Leave group (not in group or does not exist)', () async {
+    final http.Client client = MockClient();
+    final AuthModel am = AuthModel(
+        MainMemKeyValueStore(init: {"token": "asdfqwerty1234567890foobarbaz"}),
+        mocker: client);
+    final GroupModel gm = GroupModel(am, mocker: client);
+
+    when(client.post('${GroupModel.GROUP_URL}/2/leave',
+            headers: anyNamed("headers")))
+        .thenAnswer((_) async => http.Response(
+            json.encode(<String, dynamic>{"message": "Cannot leave group"}),
+            400));
+
+    // pretend client obtained `g` was from `getGroup` or `getGroups`
+    Group g = Group.fromJson(<String, dynamic>{
+      "id": 2,
+      "name": "foo",
+      "createdAt": "2020-01-01T00:00:00.000Z",
+      "updatedAt": "2020-01-01T00:00:00.000Z",
+      "defaultGroup": false,
+      "code": "BG1egA",
+      "role": "owner"
+    });
+
+    expect(() async => await gm.leaveGroup(g), throwsException);
+  });
 }
