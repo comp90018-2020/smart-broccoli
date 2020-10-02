a736a4680a32b0967e811d877297a69d4a31fc86
Steven Tang <steven@steventang.net>
2020-10-02 13:06:54 +1000

    index stack alternative

--- lib/src/Login/login_screen.dart
+++ lib/src/Login/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './login.dart';
 import './register.dart';
+import './index_stack.dart';
 
 /// Use : The Login Screen provides an interface to verify the user and log them
 /// into the application.
@@ -15,6 +16,8 @@ class LoginScreen extends StatefulWidget {
 enum FormType { login, register }
 
 class _LoginScreenState extends State<LoginScreen> {
+  int _tabIndex = 0;
+
   @override
   void initState() {
     super.initState();
@@ -23,54 +26,56 @@ class _LoginScreenState extends State<LoginScreen> {
   // Primary start up function
   @override
   Widget build(BuildContext context) {
-    Login login = Login();
-    Register register = Register();
-
     // Create a new Scaffold
     return new Scaffold(
-        body: DefaultTabController(
-            length: 2,
-            child: Column(
-              children: <Widget>[
-                // Build title (logo/application name)
-                Container(
-                    height: 200,
-                    color: Colors.white,
-                    child: Center(
-                      child: Image(
-                          image:
-                              AssetImage('assets/images/Logo_Placeholder.png')),
-                    )),
+        body: SingleChildScrollView(
+            child: DefaultTabController(
+                length: 2,
+                child: Column(
+                  children: <Widget>[
+                    // Build title (logo/application name)
+                    Container(
+                        height: 200,
+                        color: Colors.white,
+                        child: Center(
+                          child: Image(
+                              image: AssetImage(
+                                  'assets/images/Logo_Placeholder.png')),
+                        )),
 
-                // Tabs
-                FractionallySizedBox(
-                    widthFactor: 0.5,
-                    child: Container(
-                        margin: EdgeInsets.only(top: 35, bottom: 20),
-                        decoration: BoxDecoration(
-                            color: Color(0xFF82C785),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(25))),
-                        child: TabBar(
-                          tabs: [
-                            new Tab(
-                              text: "LOGIN",
-                            ),
-                            new Tab(
-                              text: "SIGN UP",
-                            )
-                          ],
-                        ))),
+                    // Tabs
+                    FractionallySizedBox(
+                        widthFactor: 0.5,
+                        child: Container(
+                            margin: EdgeInsets.only(top: 35, bottom: 20),
+                            decoration: BoxDecoration(
+                                color: Color(0xFF82C785),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(25))),
+                            child: TabBar(
+                              onTap: (value) {
+                                setState(() {
+                                  _tabIndex = value;
+                                });
+                              },
+                              tabs: [
+                                new Tab(
+                                  text: "LOGIN",
+                                ),
+                                new Tab(
+                                  text: "SIGN UP",
+                                )
+                              ],
+                            ))),
 
-                // Tab contents
-                Expanded(
-                    child: FractionallySizedBox(
-                  widthFactor: 0.7,
-                  child: TabBarView(
-                    children: [login, register],
-                  ),
-                )),
-              ],
-            )));
+                    // Tab contents
+                    FractionallySizedBox(
+                        widthFactor: 0.7,
+                        child: AnimatedIndexedStack(
+                          index: _tabIndex,
+                          children: [Login(), Register()],
+                        ))
+                  ],
+                ))));
   }
 }
