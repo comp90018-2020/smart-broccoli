626801595f034530b5a64051eea764d9d2bd9ed5
Alan Ung <alanu@student.unimelb.edu.au>
2020-10-03 14:05:17 +1000

    Extract ThemeData out of main module

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:fuzzy_broccoli/theme.dart';
+
 import 'src/auth/auth_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -10,42 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Fuzzy Broccoli',
-
-      /// A issue I've encountered is that not everything can be put into
-      /// Theme Data, since out buttons are not all the same
-      /// Also for some weird reason, padding also needs to be added
-      /// Manually, hopefully a solution can be found soon.
-      theme: ThemeData(
-        buttonTheme: ButtonThemeData(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(6))),
-            padding: EdgeInsets.symmetric(vertical: 15),
-            buttonColor: Color(0xFFFEC12D),
-            textTheme: ButtonTextTheme.accent,
-            // https://stackoverflow.com/questions/56194168
-            colorScheme: Theme.of(context)
-                .colorScheme
-                .copyWith(secondary: Color(0xFF755915))),
-        textTheme: TextTheme(
-            button: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-            subtitle1: TextStyle(
-              fontSize: 14,
-              // color: Color(0xFFAEAEAE)
-            )),
-        inputDecorationTheme: InputDecorationTheme(
-            contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            filled: true,
-            fillColor: Colors.white,
-            border: UnderlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(5)))),
-        tabBarTheme: TabBarTheme(
-            labelColor: Color(0xFF654C12),
-            unselectedLabelColor: Colors.white,
-            indicator: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: Color(0xFFFEC12D))),
-        scaffoldBackgroundColor: Color(0xFF4CAF50),
-      ),
+      theme: FuzzyBroccoliTheme().themeData,
       routes: {'/auth': (context) => AuthScreen()},
       initialRoute: '/auth',
     );
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,5 +1,50 @@
 import 'package:flutter/material.dart';
 
+/// Singleton class holding the app's `ThemeData` object
+class FuzzyBroccoliTheme {
+  static final _instance = FuzzyBroccoliTheme._internal();
+  final ThemeData _themeData = ThemeData(
+    buttonTheme: ButtonThemeData(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(6)),
+      ),
+      padding: EdgeInsets.symmetric(vertical: 15),
+      buttonColor: Color(0xFFFEC12D),
+      textTheme: ButtonTextTheme.primary,
+    ),
+    textTheme: TextTheme(
+      button: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+      subtitle1: TextStyle(
+        fontSize: 14,
+        // color: Color(0xFFAEAEAE)
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      filled: true,
+      fillColor: Colors.white,
+      border: UnderlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      ),
+    ),
+    tabBarTheme: TabBarTheme(
+      labelColor: Color(0xFF654C12),
+      unselectedLabelColor: Colors.white,
+      indicator: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        color: Color(0xFFFEC12D),
+      ),
+    ),
+    scaffoldBackgroundColor: Color(0xFF4CAF50),
+  );
+  ThemeData get themeData => _themeData;
+
+  FuzzyBroccoliTheme._internal();
+  factory FuzzyBroccoliTheme() {
+    return _instance;
+  }
+}
+
 /// Widget to hold the app logo on auth screen
 class LogoContainer extends Container {
   LogoContainer({@required Widget child})
