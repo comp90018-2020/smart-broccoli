18d0359e276b17148ab0705a8e2a977648830661
Alan Ung <alanu@student.unimelb.edu.au>
2020-10-03 19:02:31 +1000

    Delete old theme.dart module

--- lib/src/theme/theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Theme {
-  static ThemeData getThemeData() {
-    return new ThemeData(
-      scaffoldBackgroundColor: Color(0xFF4CAF50),
-    );
-  }
-}
