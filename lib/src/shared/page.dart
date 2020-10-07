5ee4273c946e3c15e997da179cd1c3ef34f31d0a
Steven Tang <steven@steventang.net>
2020-10-07 13:06:20 +1100

    UI: Custom page (#68)
    * wip: custom page
    * add const to text
    * add drawer
    * make drawer header clickable
    * delete unused import
    * fix: change to gesture detector
    * separate page from tabbedpage
    * remove chevron for logout
    * Update lib/src/shared/tabbed_page.dart
    Co-authored-by: Alan Ung <alanu@student.unimelb.edu.au>
    * ref: CustomTabPage
    
    Co-authored-by: Alan Ung <alanu@student.unimelb.edu.au>

---
+++ lib/src/shared/page.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// A page extending scaffold
+/// Supports tabs, drawer
+class CustomPage extends StatelessWidget {
+  /// Title of page
+  final String title;
+
+  /// Child
+  final Widget child;
+
+  /// Whether page has drawer
+  final bool hasDrawer;
+
+  /// Constructs a custom page
+  CustomPage(
+      {@required this.title, @required this.child, this.hasDrawer = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Appbar
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: Container(
+            // Alter shadow: https://stackoverflow.com/questions/54554569
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(color: Colors.white, offset: const Offset(0, .2))
+            ]),
+            child: AppBar(
+              title: Text(this.title),
+              centerTitle: true,
+              elevation: 0,
+            )),
+      ),
+
+      // Drawer (or hamberger menu)
+      drawer: hasDrawer
+          ? Drawer(
+              child: ListView(
+                // Important: Remove any padding from the ListView.
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  SizedBox(
+                    height: 130,
+                    child: DrawerHeader(
+                      child: Column(
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  // User picture
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        color: Colors.green,
+                                        borderRadius:
+                                            BorderRadius.circular(100)),
+                                    width: 50,
+                                    height: 50,
+                                  ),
+                                  // Name/email
+                                  Expanded(
+                                      child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 18),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text('name',
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyText1),
+                                        Text('email',
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyText2),
+                                      ],
+                                    ),
+                                  )),
+                                  Icon(Icons.chevron_right,
+                                      color: Colors.grey[700])
+                                ]),
+                          ),
+                          Expanded(child: Container())
+                        ],
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    dense: true,
+                    leading: const Icon(Icons.question_answer),
+                    title: Text('Take Quiz',
+                        style:
+                            TextStyle(color: Theme.of(context).primaryColor)),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    dense: true,
+                    leading: const Icon(Icons.edit),
+                    title: Text('Manage Quiz',
+                        style:
+                            TextStyle(color: Theme.of(context).primaryColor)),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    dense: true,
+                    leading: const Icon(Icons.people),
+                    title: Text('Groups',
+                        style:
+                            TextStyle(color: Theme.of(context).primaryColor)),
+                    onTap: () {},
+                  ),
+                  Divider(),
+                  ListTile(
+                    dense: true,
+                    leading: const Icon(Icons.info_outline),
+                    title: Text('About',
+                        style: TextStyle(color: Colors.grey[700])),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    dense: true,
+                    leading: const Icon(Icons.exit_to_app),
+                    title: Text('Sign out',
+                        style: TextStyle(color: Colors.grey[700])),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            )
+          : null,
+
+      // Body of page
+      body: child,
+    );
+  }
+}
---
+++ lib/src/shared/tabbed_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:smart_broccoli/src/shared/page.dart';
+import 'package:smart_broccoli/theme.dart';
+
+/// A tabbed page
+class CustomTabbedPage extends CustomPage {
+  /// Constructs a custom page
+  ///
+  /// Tabs (the toggle elements) and tab views (the tabs/subpages) should be in
+  /// `tabs` and `tabViews` respectively.
+  CustomTabbedPage(
+      {@required String title,
+      @required List<Tab> tabs,
+      @required List<Widget> tabViews,
+      hasDrawer = false})
+      : super(
+          title: title,
+          child: DefaultTabController(
+            length: tabs.length,
+            child: Column(children: [
+              // Add tabs
+              PreferredSize(
+                preferredSize: Size.fromHeight(kToolbarHeight + 38),
+                child: Container(
+                  padding: const EdgeInsets.only(top: 26, bottom: 12),
+                  child: TabHolder(
+                    widthFactor: .8,
+                    margin: EdgeInsets.zero,
+                    tabs: tabs,
+                  ),
+                ),
+              ),
+              // Tab body
+              Expanded(
+                child: TabBarView(
+                  children: tabViews,
+                ),
+              )
+            ]),
+          ),
+          hasDrawer: hasDrawer,
+        );
+}
