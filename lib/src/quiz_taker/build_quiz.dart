9fe16fe052eb4c4d589ce30cf922562875f7a169
Steven Tang <steven@steventang.net>
2020-10-12 14:44:35 +1100

    ref: take quiz

--- lib/src/quiz_taker/build_quiz.dart
+++ lib/src/quiz_taker/build_quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_broccoli/src/quiz_taker/quiz_card.dart';
 
 // import 'package:smart_broccoli/src/shared/background.dart';
 import 'package:smart_broccoli/src/quiz_taker/start_lobby.dart';
@@ -23,129 +24,14 @@ class _BuildQuiz extends State<BuildQuiz> {
   @override
   Widget build(BuildContext context) {
     List<String> items = getItems();
-    return Column(
-      children: <Widget>[
-        new Container(
-          child: _pinForm(),
-        ),
-        SizedBox(height: 10),
-        new FractionallySizedBox(
-          widthFactor: 0.5,
-          child: Text(
-              "By entering PIN you can access a quiz and join into the group of the that quiz"),
-        ),
-        // Padding
-        SizedBox(height: 50),
-        // Join by pin button
-        new Container(
-          child: _buildJoinByPinButton(),
-        ),
-        // Padding
-        SizedBox(height: 50),
 
-        Container(
-          child: Expanded(
-            //width: 200,
-            child: ListView.separated(
-              // Enable Horizontal Scroll
-              scrollDirection: Axis.horizontal,
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                return _cardTile(items[index], 'A', index.toString());
-              },
-              // Space between the cards
-              separatorBuilder: (context, index) {
-                return Divider(indent: 1);
-              },
-            ),
-          ),
-        ),
-        SizedBox(height: 80),
-      ],
-    );
-  }
-
-  /// This is a single tile within a list
-  /// val is the position ID
-  /// type is one of "ALL" "SELF" "LIVE"
-  /// This is used to demostrate that tab changes the list as well
-  Widget _cardTile(String val, String type, String index) {
-    double width = MediaQuery.of(context).size.width;
-    // double height = MediaQuery.of(context).size.height;
-
-    return new Container(
-      key: Key(index + val + index),
-      //height: 150,
-      //  controls the width of cards
-      width: width * 0.4,
-      child: Card(
-        elevation: 2,
-        child: InkWell(
-          // For cool color effects uncomment these two lines
-          // highlightColor: Colors.pinkAccent,
-          // splashColor: Colors.greenAccent,
-          onTap: () => _quiz(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              pictureWithTitle(val, type),
-              status(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  // You can put a picture and title in this widget
-  Widget pictureWithTitle(val, type) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        // The image here is a placeholder, it is necessary to
-        // Provide a height and a width value
-        Container(
-          height: height * 0.2,
-          width: width * 0.2,
-          // child: Image(image: AssetImage('assets/images/placeholder.png')),
-        ),
-        Text(
-          val + type,
-          style: TextStyle(fontSize: 20),
-        ),
-        Text('Subtitle', style: TextStyle(fontSize: 15)),
-      ],
-    );
-  }
-
-  Widget status() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        Text('Live', style: TextStyle(fontSize: 15)),
-      ],
-    );
-  }
-
-  /// Take a quiz, goes to the quiz lobby which then connects you to a quiz
-  /// Interface
-  void _quiz() {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (context) => StartLobby()),
-    );
-  }
-
-  // The form which you enter the pin into
-  Widget _pinForm() {
-    return new Container(
-      padding: EdgeInsets.fromLTRB(150, 16, 150, 0),
-      child: new Column(
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
         children: <Widget>[
-          new Container(
+          // Join by pin box
+          ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 100),
             child: TextFormField(
               controller: _pinFilter,
               decoration: new InputDecoration(
@@ -157,23 +43,68 @@ class _BuildQuiz extends State<BuildQuiz> {
               onFieldSubmitted: (_) => FocusScope.of(context).nextFocus(),
             ),
           ),
+
+          // Join by pin button
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: RaisedButton(
+              onPressed: _verifyPin,
+              child: Text("JOIN BY PIN"),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 8, bottom: 20.0),
+            child: FractionallySizedBox(
+              widthFactor: 0.8,
+              child: Text(
+                "By entering PIN you can access a quiz\n and join the group of that quiz",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+
+          Expanded(
+            // Reason for Center
+            // https://stackoverflow.com/questions/54126018
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.only(left: 25),
+                constraints: BoxConstraints(maxHeight: 290),
+                child: ListView.separated(
+                  // Enable Horizontal Scroll
+                  scrollDirection: Axis.horizontal,
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        child: QuizCard(
+                          'Quiz name',
+                          'Group name',
+                          onTap: _quiz,
+                        ));
+                  },
+                  // Space between the cards
+                  separatorBuilder: (context, index) {
+                    return Divider(indent: 1);
+                  },
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 
-  /// The Join by Pin button
-  /// Currently it points towards a question tab
-  Widget _buildJoinByPinButton() {
-    return new Container(
-      child: new Column(
-        children: <Widget>[
-          RaisedButton(
-            onPressed: _verifyPin, // TODO CHANGE
-            child: Text("Join By Pin"),
-          ),
-        ],
-      ),
+  /// Take a quiz, goes to the quiz lobby which then connects you to a quiz
+  /// Interface
+  void _quiz() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => StartLobby()),
     );
   }
 
---
+++ lib/src/quiz_taker/quiz_card.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+/// Represents a quiz card
+class QuizCard extends StatefulWidget {
+  /// Tap function
+  final void Function() onTap;
+
+  // TODO: change attributes to Group/Quiz
+  /// Quiz name
+  final String _quizName;
+
+  /// Group name
+  final String _groupName;
+
+  QuizCard(this._quizName, this._groupName, {Key key, this.onTap});
+
+  @override
+  State<StatefulWidget> createState() => new _QuizCardState();
+}
+
+class _QuizCardState extends State<QuizCard> {
+  Widget build(BuildContext context) {
+    Widget card = Card(
+      elevation: 2,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          // Quiz picture
+          AspectRatio(aspectRatio: 1.4, child: Placeholder()),
+
+          // Rest should expand vertically
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              width: double.maxFinite,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Quiz title & Group name
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget._quizName,
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(widget._groupName, style: TextStyle(fontSize: 15)),
+                      ],
+                    ),
+                  ),
+
+                  // Quiz status
+                  Text('Live', style: TextStyle(fontSize: 15)),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    return widget.onTap != null
+        ? InkWell(
+            // For cool color effects uncomment these two lines
+            // highlightColor: Colors.pinkAccent,
+            // splashColor: Colors.greenAccent,
+            onTap: widget.onTap,
+            child: card)
+        : card;
+  }
+}
