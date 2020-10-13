2c01229fcd04aa62ea0aa9379ec6b3eabf829595
Qifan Deng <plocircle@live.com>
2020-10-13 22:03:21 +0800

    get userinfo from decrypted token

--- backend/game/index.ts
+++ backend/game/index.ts
@@ -1,49 +1,52 @@
-import { Server, Namespace } from 'socket.io';
-import { LiveQuiz } from './quiz';
+import { Server } from 'socket.io';
+import { Conn, LiveQuiz } from './quiz';
 
-
-
-let namespace: Namespace;
-const handler = new LiveQuiz();
+export const handler:LiveQuiz = new LiveQuiz();
 
 export default (socketIO: Server) => {
     socketIO.use(async (socket, next) => {
         // check socket.handshake contents (authentication)
-        if (handler.verifySocketConn(socket)) {
+        try{
+            const conn: Conn = await handler.verifySocket(socket);
             // join & welcome
-            handler.welcome(socket);
+            handler.welcome(socket, conn);
 
             // answer
             socket.on('answer', (content: any) => {
-                handler.answer(socket, content);
+                handler.answer(socket, content, conn);
             });
 
             // quit
             socket.on('quit', (content: any) => {
-                handler.quit(socket, content);
+                handler.quit(socket, content, conn);
             });
 
             // start
             socket.on('start', (content: any) => {
-                handler.start(socket, content);
+                handler.start(socket, content, conn);
             });
 
             // abort
             socket.on('abort', (content: any) => {
-                handler.abort(socket, content);
+                handler.abort(socket, content, conn);
             });
 
             // next question
             socket.on('next', (content: any) => {
-                handler.nextQuestion(socket);
+                handler.nextQuestion(socket, conn);
             });
 
             // showBoard
             socket.on('showBoard', (content: any) => {
-                handler.showBoard(socket, content);
+                handler.showBoard(socket, content, conn);
             });
 
-        } else {
+        }
+        catch(err){
+            if(process.env.NODE_EVN === 'debug'){
+                // https://stackoverflow.com/questions/18391212
+                socket.send(JSON.stringify(err, Object.getOwnPropertyNames(err)));
+            }
             socket.disconnect();
         };
 
@@ -51,5 +54,4 @@ export default (socketIO: Server) => {
     })
 
 
-    namespace = socketIO.of('/');
 }
