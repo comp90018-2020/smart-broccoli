5525a08700fb9097063a2ba357904e0086e43360
Steven Tang <steven@steventang.net>
2020-10-14 22:46:53 +1100

    ref: use ios buttons for top

--- lib/src/quiz_creator/question_creator.dart
+++ lib/src/quiz_creator/question_creator.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:image_picker/image_picker.dart';
+import 'package:smart_broccoli/src/shared/page.dart';
 
 class QuestionCreate extends StatefulWidget {
   QuestionCreate({Key key}) : super(key: key);
@@ -13,6 +15,147 @@ class QuestionCreate extends StatefulWidget {
 }
 
 class _QuestionCreateState extends State<QuestionCreate> {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(),
+        () => SystemChannels.textInput.invokeMethod('TextInput.hide'));
+
+    return CustomPage(
+      title: 'Question',
+      secondaryBackgroundColour: true,
+
+      // Close icon
+      appbarLeading: GestureDetector(
+        onTap: () {},
+        child: Icon(Icons.close),
+      ),
+
+      // Delete and Save on AppBar
+      appbarActions: <Widget>[
+        CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {},
+            child: Icon(Icons.delete)),
+        CupertinoButton(
+          padding: EdgeInsets.only(right: 12),
+          onPressed: () {},
+          child: Text(
+            'Save',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ],
+
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+                padding: EdgeInsets.fromLTRB(12.00, 10.00, 0, 3.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Text(
+                      'Question X',
+                      style: new TextStyle(
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                )),
+            Padding(
+                padding: EdgeInsets.fromLTRB(12.0, 8, 10.0, 0.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Question text',
+                  ),
+                )),
+            Container(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Container(
+                        width: 390,
+                        child: Card(
+                          semanticContainer: true,
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          child: Column(children: <Widget>[
+                            Container(
+                              width: 380,
+                              height: 100,
+                              child: _decideImageView(),
+                            ),
+                            ButtonBar(
+                              alignment: MainAxisAlignment.center,
+                              children: [
+                                FlatButton(
+                                  textColor: Colors.black54,
+                                  onPressed: () {
+                                    _showChoiceDialog(context);
+                                    // Perform some action
+                                  },
+                                  child: const Text('SET QUESTION IMAGE'),
+                                ),
+                              ],
+                            )
+                          ]),
+                          shape: RoundedRectangleBorder(),
+                          elevation: 5,
+                          margin: EdgeInsets.fromLTRB(12.0, 8, 10.0, 0.0),
+                        )),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(12.0, 8, 10.0, 0.0),
+            ),
+            Container(
+                padding: EdgeInsets.fromLTRB(12.00, 10.00, 0, 3.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Text(
+                      'Answers',
+                      style: new TextStyle(
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                )),
+            Column(
+              children: <Widget>[
+                ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: cards.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return cards[index];
+                  },
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: RaisedButton(
+                    padding: EdgeInsets.all(14.0),
+                    child: Text('ADD ANSWER'),
+                    onPressed: () => setState(() => cards.add(createCard())),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   // Answer cards START
   // Creates an answer card
 
@@ -112,138 +255,4 @@ class _QuestionCreateState extends State<QuestionCreate> {
 
   // Set image END
 
-  @override
-  Widget build(BuildContext context) {
-    Future.delayed(const Duration(),
-        () => SystemChannels.textInput.invokeMethod('TextInput.hide'));
-
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: SizedBox(
-          child: Text("Question"),
-        ),
-        leading: GestureDetector(
-          onTap: () {},
-          child: Icon(Icons.close),
-        ),
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 20.0, top: 0.0),
-            child: GestureDetector(onTap: () {}, child: Icon(Icons.delete)),
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 20.0, top: 22.0),
-            child: GestureDetector(onTap: () {}, child: Text("SAVE")),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Container(
-              padding: EdgeInsets.fromLTRB(12.00, 10.00, 0, 3.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  new Text(
-                    'Settings',
-                    style: new TextStyle(
-                      fontSize: 17.0,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white,
-                    ),
-                  )
-                ],
-              )),
-          Padding(
-              padding: EdgeInsets.fromLTRB(12.0, 8, 10.0, 0.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Question text',
-                ),
-              )),
-          Container(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Container(
-                      width: 390,
-                      child: Card(
-                        semanticContainer: true,
-                        clipBehavior: Clip.antiAliasWithSaveLayer,
-                        child: Column(children: <Widget>[
-                          Container(
-                            width: 380,
-                            height: 100,
-                            child: _decideImageView(),
-                          ),
-                          ButtonBar(
-                            alignment: MainAxisAlignment.center,
-                            children: [
-                              FlatButton(
-                                textColor: Colors.black54,
-                                onPressed: () {
-                                  _showChoiceDialog(context);
-                                  // Perform some action
-                                },
-                                child: const Text('SET QUESTION IMAGE'),
-                              ),
-                            ],
-                          )
-                        ]),
-                        shape: RoundedRectangleBorder(),
-                        elevation: 5,
-                        margin: EdgeInsets.fromLTRB(12.0, 8, 10.0, 0.0),
-                      )),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(12.0, 8, 10.0, 0.0),
-          ),
-          Container(
-              padding: EdgeInsets.fromLTRB(12.00, 10.00, 0, 3.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  new Text(
-                    'Answers',
-                    style: new TextStyle(
-                      fontSize: 17.0,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              )),
-          Column(
-            children: <Widget>[
-              ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemCount: cards.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return cards[index];
-                },
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: RaisedButton(
-                  padding: EdgeInsets.all(14.0),
-                  child: Text('ADD ANSWER'),
-                  onPressed: () => setState(() => cards.add(createCard())),
-                ),
-              )
-            ],
-          ),
-        ],
-      )),
-    );
-  }
 }
