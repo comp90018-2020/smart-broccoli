bd8dbe7a2beeb454000e36acb0a623f8a12f971a
Alan Ung <alanu@student.unimelb.edu.au>
2020-10-15 21:14:04 +1100

    Missed one

--- lib/src/quiz_creator/quiz_creator.dart
+++ lib/src/quiz_creator/quiz_creator.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
 import 'package:smart_broccoli/theme.dart';
 
 import '../data/quiz.dart';
 import '../shared/page.dart';
-import 'package:numberpicker/numberpicker.dart';
 
 import 'picture.dart';
 
