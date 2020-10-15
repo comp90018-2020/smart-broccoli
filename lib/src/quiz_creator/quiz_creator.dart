efbb034163b0c0e957ca7b7fbd277d1c7aa66d11
Steven Tang <steven@steventang.net>
2020-10-15 20:02:37 +1100

    ref: quiz page

--- lib/src/quiz_creator/quiz_creator.dart
+++ lib/src/quiz_creator/quiz_creator.dart
@@ -1,9 +1,10 @@
-import 'dart:io';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:smart_broccoli/src/quiz_creator/picture.dart';
+import 'package:smart_broccoli/src/shared/page.dart';
+import 'package:smart_broccoli/theme.dart';
 
 import '../data/quiz.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:numberpicker/numberpicker.dart';
 
 class QuizCreate extends StatefulWidget {
@@ -14,272 +15,206 @@ class QuizCreate extends StatefulWidget {
 }
 
 class _QuizCreateState extends State<QuizCreate> {
-  // Timer picker START
-  int _currentIntValue = 30;
-  NumberPicker integerNumberPicker;
+  // Key for form
+  final _formKey = GlobalKey<FormState>();
 
-  var txt = TextEditingController();
+  // Text controller for seconds per question
+  var timerTextController = TextEditingController();
 
-  _handleValueChangedExternally(num value) {
-    if (value != null) {
-      if (value is int) {
-        setState(() {
-          _currentIntValue = value;
-          txt.text = _currentIntValue.toString() + " seconds";
-        });
-      }
-    }
+  // TODO: replace with cloned quiz
+  Quiz model = Quiz("placeholder", 0, QuizType.LIVE);
+
+  // The current picked file
+  String picturePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPage(
+      title: "Quiz",
+      secondaryBackgroundColour: true,
+
+      // Close icon
+      appbarLeading: GestureDetector(
+        onTap: () {},
+        child: Icon(Icons.close),
+      ),
+
+      // Delete and Save on AppBar
+      appbarActions: <Widget>[
+        CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {},
+            child: Icon(Icons.delete)),
+        CupertinoButton(
+          padding: EdgeInsets.only(right: 14),
+          onPressed: () {},
+          child: Text(
+            'Save',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ],
+
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Settings
+                Text(
+                  'Settings',
+                  style: new TextStyle(
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                ),
+
+                // Name box
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Quiz name',
+                    ),
+                  ),
+                ),
+
+                // Picture selection
+                PictureCard(picturePath, (_) {}),
+
+                // Seconds selection
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  child: TextField(
+                    autofocus: false,
+                    controller: timerTextController,
+                    readOnly: true,
+                    onTap: _showTimeDialog,
+                    decoration: InputDecoration(
+                        labelText: 'Seconds per question',
+                        prefixIcon: Icon(Icons.timer)),
+                  ),
+                ),
+
+                // Group selection
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Card(
+                    margin: EdgeInsets.zero,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 4, horizontal: 12),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.people,
+                            color: Colors.grey,
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: DropdownButton(
+                                  isExpanded: true,
+                                  items: [
+                                    DropdownMenuItem(child: Text('X')),
+                                    DropdownMenuItem(child: Text('Y'))
+                                  ],
+                                  onChanged: (_) {}),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+                // Quiz type
+                Card(
+                  margin: EdgeInsets.zero,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  child: Column(
+                    children: <Widget>[
+                      RadioListTile<QuizType>(
+                        dense: true,
+                        title: const Text('LIVE'),
+                        value: QuizType.LIVE,
+                        groupValue: model.type,
+                        onChanged: (QuizType value) {
+                          setState(() {
+                            model.type = value;
+                          });
+                        },
+                      ),
+                      RadioListTile<QuizType>(
+                        dense: true,
+                        title: const Text('SELF-PACED'),
+                        value: QuizType.SELF_PACED,
+                        groupValue: model.type,
+                        onChanged: (QuizType value) {
+                          setState(() {
+                            model.type = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+
+                // Questions title
+                Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 8),
+                  child: Text(
+                    'Questions',
+                    style: new TextStyle(
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+
+                // Add question
+                Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: RaisedButton(
+                      shape: SmartBroccoliTheme.raisedButtonShape,
+                      padding: EdgeInsets.all(14.0),
+                      child: Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          spacing: 3,
+                          children: [Icon(Icons.add), Text('ADD QUESTION')]),
+                      onPressed: () {},
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
-  Future _showIntegerDialog() async {
+  // Time dialog
+  Future _showTimeDialog() async {
     await showDialog<int>(
         context: context,
         builder: (BuildContext context) {
           return new NumberPickerDialog.integer(
               minValue: 5, maxValue: 90, initialIntegerValue: 30);
-        }).then((value) => _handleValueChangedExternally(value));
-  }
-
-  // Timer picker END**********************************/
-
-  // Image setter START**********************************/
-
-  File imageFile;
-  final picker = ImagePicker();
-
-  _openGallery(BuildContext context) async {
-    imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
-    this.setState(() {});
-    Navigator.of(context).pop();
-  }
-
-  _openCamera(BuildContext context) async {
-    imageFile = await ImagePicker.pickImage(source: ImageSource.camera);
-    this.setState(() {});
-
-    Navigator.of(context).pop();
-  }
-
-  Future<void> _showChoiceDialog(BuildContext context) {
-    return showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Select upload method"),
-            content: SingleChildScrollView(
-              child: ListBody(
-                children: <Widget>[
-                  GestureDetector(
-                    child: Text("From gallery"),
-                    onTap: () {
-                      _openGallery(context);
-                    },
-                  ),
-                  Padding(padding: EdgeInsets.all(8.0)),
-                  GestureDetector(
-                    child: Text("Using camera"),
-                    onTap: () {
-                      _openCamera(context);
-                    },
-                  )
-                ],
-              ),
-            ),
-          );
+        }).then((value) {
+      if (value != null && value is int) {
+        setState(() {
+          model.timeLimit = value;
+          timerTextController.text = value.toString() + " seconds";
         });
-  }
-
-  Widget _decideImageView() {
-    if (imageFile == null) {
-      return IconButton(
-        padding: new EdgeInsets.fromLTRB(0.0, 12.0, 0.0, 0.0),
-        icon: new Icon(Icons.insert_photo_outlined, size: 100),
-        onPressed: () {},
-      );
-    } else {
-      return Image.file(imageFile, fit: BoxFit.cover);
-    }
-  }
-
-  Quiz model = Quiz("placeholder", 0, QuizType.LIVE);
-
-  QuizType radioBtn = QuizType.LIVE;
-
-  final _formKey = GlobalKey<FormState>();
-  _QuizCreateState();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: SizedBox(
-          child: Text("Quiz"),
-        ),
-        leading: GestureDetector(
-          onTap: () {},
-          child: Icon(Icons.close),
-        ),
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 20.0, top: 0.0),
-            child: GestureDetector(onTap: () {}, child: Icon(Icons.delete)),
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 20.0, top: 22.0),
-            child: GestureDetector(onTap: () {}, child: Text("SAVE")),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              Container(
-                  padding: EdgeInsets.fromLTRB(12.00, 10.00, 0, 3.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      new Text(
-                        'Settings',
-                        style: new TextStyle(
-                          fontSize: 17.0,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
-                  )),
-              Padding(
-                  padding: EdgeInsets.fromLTRB(12.0, 8, 10.0, 0.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Quiz name',
-                    ),
-                  )),
-              Container(
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Container(
-                          width: 390,
-                          child: Card(
-                            semanticContainer: true,
-                            clipBehavior: Clip.antiAliasWithSaveLayer,
-                            child: Column(children: <Widget>[
-                              Container(
-                                width: 380,
-                                height: 100,
-                                child: _decideImageView(),
-                              ),
-                              ButtonBar(
-                                alignment: MainAxisAlignment.center,
-                                children: [
-                                  FlatButton(
-                                    textColor: Colors.black54,
-                                    onPressed: () {
-                                      _showChoiceDialog(context);
-                                      // Perform some action
-                                    },
-                                    child: const Text('SET QUIZ IMAGE'),
-                                  ),
-                                ],
-                              )
-                            ]),
-                            shape: RoundedRectangleBorder(),
-                            elevation: 5,
-                            margin: EdgeInsets.fromLTRB(12.0, 8, 10.0, 0.0),
-                          )),
-                    ],
-                  ),
-                ),
-              ),
-              Padding(
-                  padding: EdgeInsets.fromLTRB(12.0, 8, 10.0, 0.0),
-                  child: TextField(
-                    autofocus: false,
-                    controller: txt,
-                    // readOnly: true,
-                    onTap: _showIntegerDialog,
-                    decoration: InputDecoration(
-                        labelText: 'Seconds per question',
-                        prefixIcon: Icon(Icons.timer)),
-                  )),
-              Container(
-                alignment: Alignment.centerLeft,
-                margin: EdgeInsets.fromLTRB(12.0, 6.0, 10.0, 0.0),
-                height: 100,
-                child: Column(
-                  children: <Widget>[
-                    ListTile(
-                      title: const Text('LIVE'),
-                      dense: true,
-                      leading: Radio(
-                        activeColor: Colors.green,
-                        value: QuizType.LIVE,
-                        groupValue: radioBtn,
-                        onChanged: (QuizType value) {
-                          setState(() {
-                            radioBtn = value;
-                          });
-                        },
-                      ),
-                    ),
-                    ListTile(
-                      dense: true,
-                      title: const Text('SELF-PACED'),
-                      leading: Radio(
-                        activeColor: Colors.green,
-                        value: QuizType.SELF_PACED,
-                        groupValue: radioBtn,
-                        onChanged: (QuizType value) {
-                          setState(() {
-                            radioBtn = value;
-                          });
-                        },
-                      ),
-                    )
-                  ],
-                ),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                      padding: EdgeInsets.fromLTRB(12.00, 25.00, 0, 3.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          new Text(
-                            'Questions',
-                            style: new TextStyle(
-                              fontSize: 17.0,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.white,
-                            ),
-                          )
-                        ],
-                      )),
-                  const SizedBox(height: 30),
-                  RaisedButton(
-                    onPressed: () {},
-                    padding: EdgeInsets.all(20.0),
-                    child: const Text(
-                      'Add Question',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  )
-                ],
-              )
-            ],
-          )),
-    );
+      }
+    });
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -22,6 +22,7 @@ class SmartBroccoliTheme {
   static final ThemeData themeData = ThemeData(
     backgroundColor: Color(0xFF4CAF50),
     primaryColor: Color(0xFF4CAF50),
+    accentColor: Colors.green,
     colorScheme: SmartBroccoliColourScheme(),
     buttonTheme: ButtonThemeData(
       shape: RoundedRectangleBorder(
