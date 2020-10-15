f15fe4a53ba7e073be8310e21441f4a6e2d9a7b9
Qifan Deng <plocircle@live.com>
2020-10-15 16:06:07 +0800

    finish: assess answer for question and give points based on correctness, streak and rank of this question; todo: leaderboard

--- backend/game/scores.ts
+++ backend/game/scores.ts
@@ -1,27 +1,73 @@
-class AnswerRes {
+export class AnswerOutcome {
     constructor(
         readonly correct: boolean,
         readonly rank: number,
         readonly streak: number,
-        readonly rightParticipants: number,
-        readonly totalParticipants: number
+        readonly quesionNo: number
     ) { };
 }
 
-class PointsSystem {
+export class Answer {
+    constructor(
+        readonly questionNo: number,
+        readonly MCSelection: number,
+        readonly TFSelection: boolean,
+    ) { };
+}
+
+export class PointSystem {
     private readonly pointsEachQuestion = 1000;
 
-    private getFactor(ansRes: AnswerRes): number {
-        let base: number = ansRes.correct ? 1 : 0;
-        if (base !== 0) {
-            const a = ansRes.streak * 0.1;
-            const b = (1 - ansRes.rightParticipants/ansRes.totalParticipants) / 2;
-            return base * a * b;
-        } 
-        return base;
+    public answeredPlayer: Set<number> = new Set([]);
+    private rankOfNextRightAns: number = 0;
+
+    constructor(public participantCount: number) { 
+        this.setForNewQuestion();
     }
 
-    public getNewPoints(ansRes: AnswerRes): number{
-        return Math.floor(this.getFactor(ansRes) * this.pointsEachQuestion);
+    public getRankForARightAns(): number {
+        return this.rankOfNextRightAns++;
+    }
+
+    public hasAllPlayersAnswered() {
+        return this.participantCount - this.answeredPlayer.size <= 0;
+    }
+
+    private getFactor(ansRes: AnswerOutcome): number {
+        let factor: number = ansRes.correct ? 1 : 0;
+        if (factor !== 0) {
+            const factorStreak = ansRes.streak * 0.1;
+            const factorRank = (1 - ansRes.rank / this.participantCount) / 2;
+            console.log(factor, factorStreak, factorRank);
+            return factor + (factorStreak < 1 ? factorStreak : 1) + factorRank;
+        }
+        return factor;
+    }
+
+    public setForNewQuestion(){
+        this.rankOfNextRightAns = 0;
+        this.answeredPlayer = new Set([]);
+    }
+
+    public checkAns(ans: Answer, correctAns: Answer, preAnsOutcome: AnswerOutcome): AnswerOutcome {
+        if (ans.questionNo !== correctAns.questionNo) {
+            throw `This is ans for question ${ans.questionNo} not for ${correctAns.questionNo}`;
+        } else {
+            const correct = correctAns.MCSelection !== null ?
+                (ans.MCSelection === correctAns.MCSelection ? true : false) :
+                (ans.TFSelection === correctAns.TFSelection ? true : false);
+            return new AnswerOutcome(
+                correct,
+                correct ? this.getRankForARightAns() : this.participantCount,
+                correct ? preAnsOutcome.streak + 1 : 0,
+                correctAns.questionNo);
+        }
+    }
+
+    public getNewPoints(ansOutcome: AnswerOutcome): number {
+        // if (!this.hasAllPlayersAnswered()) {
+        //     throw "Wait for others to answer";
+        // }
+        return Math.floor(this.getFactor(ansOutcome) * this.pointsEachQuestion);
     }
 }
