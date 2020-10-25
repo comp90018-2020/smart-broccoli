8d5a5e2b9f01e1cd32a9e5081c914fb662fd6024
ekurmakaevMac <ekurmakaev@student.unimelb.edu.au>
2020-10-25 21:57:07 +1100

    adding new quiz works, editing does not work yet

--- lib/src/ui/quiz_creator/quiz_creator.dart
+++ lib/src/ui/quiz_creator/quiz_creator.dart
@@ -87,6 +87,7 @@ class _QuizCreateState extends State<QuizCreate> {
           child: Text(
             'Save',
             style: TextStyle(color: Colors.white, fontSize: 16),
+
           ),
         ),
       ],
