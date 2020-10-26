3073b60b6dc7f710499fb97a25baf85969cf2ff1
zhaomin <min.zhao1@student.unimelb.edu.au>
2020-10-26 11:51:36 +0800

    restyple outcome

--- lib/src/socket_data/outcome.dart
+++ lib/src/socket_data/outcome.dart
@@ -4,8 +4,8 @@ import 'record.dart';
 class Outcome {
   final int question;
   final List<UserRank> leaderboard;
-  Outcome(this.question, this.leaderboard);
-  factory Outcome.fromJson(Map<String, dynamic> json) => Outcome(
+  Outcome._internal(this.question, this.leaderboard);
+  factory Outcome.fromJson(Map<String, dynamic> json) => Outcome._internal(
         json['question'],
         json['leaderboard'].map((e) => UserRank.fromJson(e)),
       );
@@ -16,7 +16,7 @@ class OutcomeUser extends Outcome {
   final UserRank playerAhead;
   OutcomeUser._internal(
       int question, List<UserRank> leaderboard, this.record, this.playerAhead)
-      : super(question, leaderboard);
+      : super._internal(question, leaderboard);
   factory OutcomeUser.fromJson(Map<String, dynamic> json) =>
       OutcomeUser._internal(
           json['question'],
