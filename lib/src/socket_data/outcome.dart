e9b919878fccc265809eec5c97586f21da7891df
zhaomin <min.zhao1@student.unimelb.edu.au>
2020-10-26 11:49:02 +0800

    restyle outcome

--- lib/src/socket_data/outcome.dart
+++ lib/src/socket_data/outcome.dart
@@ -4,28 +4,23 @@ import 'record.dart';
 class Outcome {
   final int question;
   final List<UserRank> leaderboard;
-
-  Outcome(this.question, List leaderboard)
-      : leaderboard = leaderboard.map((e) => UserRank.fromJson(e));
-
-  factory Outcome.fromJson(Map<String, dynamic> json) =>
-      Outcome(json['question'], json['leaderboard']);
+  Outcome(this.question, this.leaderboard);
+  factory Outcome.fromJson(Map<String, dynamic> json) => Outcome(
+        json['question'],
+        json['leaderboard'].map((e) => UserRank.fromJson(e)),
+      );
 }
 
 class OutcomeUser extends Outcome {
   final Record record;
   final UserRank playerAhead;
-
   OutcomeUser._internal(
       int question, List<UserRank> leaderboard, this.record, this.playerAhead)
       : super(question, leaderboard);
-
   factory OutcomeUser.fromJson(Map<String, dynamic> json) =>
       OutcomeUser._internal(
           json['question'],
-          json['leaderboard'],
+          json['leaderboard'].map((e) => UserRank.fromJson(e)),
           Record.fromJson(json['record']),
-          (json['playerAhead'] == null)
-              ? null
-              : UserRank.fromJson(json['playerAhead']));
+          json['playerAhead'] ?? UserRank.fromJson(json['playerAhead']));
 }
