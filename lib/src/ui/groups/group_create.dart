997637399bacba7c42d4b322b7b2ac28a722b5ce
Qifan Deng <plocircle@live.com>
2020-10-28 21:46:13 +0800

    fix: vertical group creation and disable create button when the text filed is empty

--- lib/src/ui/groups/group_create.dart
+++ lib/src/ui/groups/group_create.dart
@@ -14,47 +14,56 @@ class GroupCreate extends StatefulWidget {
 
 class _GroupCreateState extends State<GroupCreate> {
   final TextEditingController controller = TextEditingController();
+  bool _isTextFormFieldEmpty = true;
   @override
   Widget build(BuildContext context) {
     return new CustomPage(
       title: "Create Group",
       hasDrawer: false,
       secondaryBackgroundColour: true,
-      child: SingleChildScrollView(
-        child: Container(
-          alignment: Alignment.topCenter,
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
-          child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 275),
-            child: FractionallySizedBox(
-              widthFactor: 0.8,
-              child: Form(
-                child: Column(
-                  children: [
-                    // Group name
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: TextFormField(
-                        controller: controller,
-                        decoration: const InputDecoration(
-                          labelText: 'Group name',
-                          prefixIcon: Icon(Icons.people),
-                        ),
-                        onFieldSubmitted: (_) => _createGroup(),
-                      ),
-                    ),
-                    // Button
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 8),
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: RaisedButton(
-                          onPressed: _createGroup,
-                          child: const Text("CREATE"),
+      child: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+            margin: EdgeInsets.only(bottom: kToolbarHeight),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: 275),
+              child: FractionallySizedBox(
+                widthFactor: 0.8,
+                child: Form(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // Group name
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: TextFormField(
+                          controller: controller,
+                          decoration: const InputDecoration(
+                            labelText: 'Group name',
+                            prefixIcon: Icon(Icons.people),
+                          ),
+                          onChanged: (value) => setState(
+                              () => _isTextFormFieldEmpty = value.isEmpty),
+                          onFieldSubmitted: (_) => _createGroup(),
                         ),
                       ),
-                    ),
-                  ],
+                      // Button
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 8),
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: RaisedButton(
+                            onPressed:
+                                _isTextFormFieldEmpty ? null : _createGroup,
+                            child: const Text("CREATE"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
