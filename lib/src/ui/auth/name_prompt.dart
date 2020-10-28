d8e7145fd5b4520a75ae547df8bb4ccbe5ec5065
Steven Tang <steven@steventang.net>
2020-10-29 08:57:08 +1100

    add singlechildscrollview

--- lib/src/ui/auth/name_prompt.dart
+++ lib/src/ui/auth/name_prompt.dart
@@ -32,65 +32,67 @@ class _NamePromptState extends State<NamePrompt> {
           )
         ],
         child: Center(
-          child: FractionallySizedBox(
-            widthFactor: 0.7,
-            child: Wrap(
-              runSpacing: 16,
-              children: [
-                Center(
-                  child: Text(
-                    'To get started, enter your name',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.onBackground,
-                      fontSize: 16,
+          child: SingleChildScrollView(
+            child: FractionallySizedBox(
+              widthFactor: 0.7,
+              child: Wrap(
+                runSpacing: 16,
+                children: [
+                  Center(
+                    child: Text(
+                      'To get started, enter your name',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onBackground,
+                        fontSize: 16,
+                      ),
                     ),
                   ),
-                ),
 
-                // name field
-                TextField(
-                  controller: _nameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Name',
-                    prefixIcon: Icon(Icons.person),
+                  // name field
+                  TextField(
+                    controller: _nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                    keyboardType: TextInputType.name,
+                    onChanged: (value) =>
+                        setState(() => _nameEmpty = value.isEmpty),
+                    onSubmitted: (value) async {
+                      if (value.isEmpty) return;
+                      AuthStateModel auth =
+                          Provider.of<AuthStateModel>(context, listen: false);
+                      UserProfileModel profile =
+                          Provider.of<UserProfileModel>(context, listen: false);
+                      await auth.join();
+                      profile.updateUser(name: _nameController.text);
+                    },
                   ),
-                  keyboardType: TextInputType.name,
-                  onChanged: (value) =>
-                      setState(() => _nameEmpty = value.isEmpty),
-                  onSubmitted: (value) async {
-                    if (value.isEmpty) return;
-                    AuthStateModel auth =
-                        Provider.of<AuthStateModel>(context, listen: false);
-                    UserProfileModel profile =
-                        Provider.of<UserProfileModel>(context, listen: false);
-                    await auth.join();
-                    profile.updateUser(name: _nameController.text);
-                  },
-                ),
 
-                // join button
-                SizedBox(
-                  width: double.infinity,
-                  child: RaisedButton(
-                    child: const Text("JOIN"),
-                    disabledColor: Theme.of(context).disabledColor,
-                    disabledTextColor:
-                        SmartBroccoliColourScheme.disabledButtonTextColor,
-                    onPressed: _nameEmpty
-                        ? null
-                        : () async {
-                            AuthStateModel auth = Provider.of<AuthStateModel>(
-                                context,
-                                listen: false);
-                            UserProfileModel profile =
-                                Provider.of<UserProfileModel>(context,
-                                    listen: false);
-                            await auth.join();
-                            profile.updateUser(name: _nameController.text);
-                          },
+                  // join button
+                  SizedBox(
+                    width: double.infinity,
+                    child: RaisedButton(
+                      child: const Text("JOIN"),
+                      disabledColor: Theme.of(context).disabledColor,
+                      disabledTextColor:
+                          SmartBroccoliColourScheme.disabledButtonTextColor,
+                      onPressed: _nameEmpty
+                          ? null
+                          : () async {
+                              AuthStateModel auth = Provider.of<AuthStateModel>(
+                                  context,
+                                  listen: false);
+                              UserProfileModel profile =
+                                  Provider.of<UserProfileModel>(context,
+                                      listen: false);
+                              await auth.join();
+                              profile.updateUser(name: _nameController.text);
+                            },
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
