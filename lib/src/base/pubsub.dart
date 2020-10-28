684d8d469b56e4808cfcb554195522c450962827
Steven Tang <steven@steventang.net>
2020-10-28 19:15:42 +1100

    ref: force argument of function in pubsub

--- lib/src/base/pubsub.dart
+++ lib/src/base/pubsub.dart
@@ -3,21 +3,21 @@ enum PubSubTopic { ROUTE }
 
 /// Base class for publish subscribe class
 abstract class PubSubBase {
-  void subscribe(PubSubTopic topic, Function callback);
-  void unsubscribe(PubSubTopic topic, Function callback);
-  void publish(PubSubTopic topic, {dynamic arg});
+  void subscribe(PubSubTopic topic, Function(dynamic) callback);
+  void unsubscribe(PubSubTopic topic, Function(dynamic) callback);
+  void publish(PubSubTopic topic, dynamic arg);
 }
 
 /// Publish/subscribe
 class PubSub extends PubSubBase {
   /// Channels, which holds a list of functions for subscribers which are
   /// called with a topic is emitted.
-  Map<PubSubTopic, List<Function>> _channels = {};
+  Map<PubSubTopic, List<Function(dynamic)>> _channels = {};
 
   PubSub();
 
   /// Subscribe to topic
-  void subscribe(PubSubTopic topic, Function callback) {
+  void subscribe(PubSubTopic topic, Function(dynamic) callback) {
     // Initialise list
     if (!_channels.containsKey(topic)) {
       _channels[topic] = [];
@@ -26,23 +26,19 @@ class PubSub extends PubSubBase {
   }
 
   /// Unsubscribe from topic
-  void unsubscribe(PubSubTopic topic, Function callback) {
+  void unsubscribe(PubSubTopic topic, Function(dynamic) callback) {
     if (!_channels.containsKey(topic)) return;
     _channels[topic].remove(callback);
   }
 
   /// Publish topic
-  void publish(PubSubTopic topic, {dynamic arg}) {
+  void publish(PubSubTopic topic, dynamic arg) {
     // No subscribers
     if (!_channels.containsKey(topic)) return;
 
     // Call functions
     for (Function func in _channels[topic]) {
-      if (arg != null) {
-        func(arg);
-      } else {
-        func();
-      }
+      func(arg);
     }
   }
 }
