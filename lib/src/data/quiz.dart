5a3757ed3ccbec895063a505e72cdc460357c045
Qifan Deng <plocircle@live.com>
2020-10-31 09:57:27 +0800

    fix: parse null

--- lib/src/data/quiz.dart
+++ lib/src/data/quiz.dart
@@ -66,7 +66,7 @@ class Quiz implements Comparable<Quiz> {
     });
     Quiz quiz = Quiz._internal(
         json['id'],
-        DateTime.parse(json['updatedAt']) == null
+        json['updatedAt'] == null
             ? DateTime.parse('2020-01-01 00:00:00.000')
             : DateTime.parse(json['updatedAt']),
         json['pictureId'],
