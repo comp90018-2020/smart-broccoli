36c6ee4d54165604628b096e7795ab205297b178
MIA <63494004+zhaominer10@users.noreply.github.com>
2020-10-31 18:12:05 +0800

    UI: profile promoting hint and table changes (#238)
    * change import file into ui.dart
    
    * add field hins in profile_promoting.dart
    
    * change outlook of the table

--- lib/src/ui/profile/table_items.dart
+++ lib/src/ui/profile/table_items.dart
@@ -9,8 +9,8 @@ class TableCard extends Material {
           child: Table(
             defaultVerticalAlignment: TableCellVerticalAlignment.middle,
             columnWidths: const {
-              0: FlexColumnWidth(0.3),
-              1: FlexColumnWidth(0.7)
+              0: IntrinsicColumnWidth(),
+              1: FlexColumnWidth(1)
             },
             border: TableBorder.all(width: 0.8, color: Colors.black12),
             children: children,
@@ -21,15 +21,13 @@ class TableCard extends Material {
 /// Name row
 class NameTableRow extends TableRow {
   NameTableRow(bool isEdit, TextEditingController _nameController,
-      {String hintText,
+      {String hintText = 'Name',
       TextInputAction textInputAction,
       void Function(String) onFieldSubmitted})
       : super(
           children: [
-            _paddedCell(
-                const Text('NAME',
-                    style: const TextStyle(color: Colors.black38)),
-                padding: const EdgeInsets.only(left: 16)),
+            _paddedCell(Icon(Icons.person),
+                padding: EdgeInsets.symmetric(horizontal: 16)),
             _paddedCell(
               TextFormField(
                 readOnly: !isEdit,
@@ -58,14 +56,13 @@ class NameTableRow extends TableRow {
 /// Email row
 class EmailTableRow extends TableRow {
   EmailTableRow(bool isEdit, TextEditingController _emailController,
-      {String hintText,
+      {String hintText = 'Email',
       TextInputAction textInputAction,
       void Function(String) onFieldSubmitted})
       : super(
           children: [
-            _paddedCell(
-                const Text('EMAIL', style: TextStyle(color: Colors.black38)),
-                padding: const EdgeInsets.only(left: 16)),
+            _paddedCell(Icon(Icons.email),
+                padding: EdgeInsets.symmetric(horizontal: 16)),
             _paddedCell(
               TextFormField(
                 textAlignVertical: TextAlignVertical.center,
@@ -100,9 +97,8 @@ class PasswordTableRow extends TableRow {
       void Function(String) onFieldSubmitted})
       : super(
           children: [
-            _paddedCell(
-                Text('Password', style: const TextStyle(color: Colors.black38)),
-                padding: const EdgeInsets.only(left: 16)),
+            _paddedCell(Icon(Icons.lock),
+                padding: EdgeInsets.symmetric(horizontal: 16)),
             _paddedCell(
               TextFormField(
                 obscureText: true,
@@ -137,10 +133,8 @@ class PasswordConfirmTableRow extends TableRow {
       void Function(String) onFieldSubmitted})
       : super(
           children: [
-            _paddedCell(
-                Text('Confirm Password',
-                    style: const TextStyle(color: Colors.black38)),
-                padding: const EdgeInsets.only(left: 16)),
+            _paddedCell(Icon(Icons.lock),
+                padding: EdgeInsets.symmetric(horizontal: 16)),
             _paddedCell(
               TextFormField(
                 obscureText: true,
