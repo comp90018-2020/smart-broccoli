b10dc69bd4f9bd874d2b21e20b7c1a529d64bd58
Alan Ung <alanu@student.unimelb.edu.au>
2020-11-02 21:43:06 +1100

    Add TimerWidget

---
+++ lib/src/ui/session/timer.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:smart_broccoli/src/base.dart';
+import 'package:smart_broccoli/src/models/session_model.dart';
+
+class TimerWidget extends StatefulWidget {
+  @override
+  _TimerWidgetState createState() => _TimerWidgetState();
+}
+
+class _TimerWidgetState extends State<TimerWidget> {
+  Timer _timer;
+  int _secondsRemaining;
+
+  void reset(dynamic seconds) {
+    _secondsRemaining = seconds;
+    if (_timer == null)
+      _timer = Timer.periodic(
+        const Duration(seconds: 1),
+        (Timer timer) => setState(() {
+          if (_secondsRemaining < 1)
+            timer.cancel();
+          else
+            --_secondsRemaining;
+        }),
+      );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<GameSessionModel>(context, listen: false)
+        .pubSub
+        .subscribe(PubSubTopic.TIMER, reset);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    Provider.of<GameSessionModel>(context, listen: false)
+        .pubSub
+        .unsubscribe(PubSubTopic.TIMER, reset);
+    _timer?.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('$_secondsRemaining');
+  }
+}
