43cf78df29918b3df87f1ea445b0fbc87ed1f958
Mia <zhaominer10@gmail.com>
2020-11-04 08:48:42 +0800

    finish all except days of week

--- lib/src/ui/quiz/smart_live_quiz.dart
+++ lib/src/ui/quiz/smart_live_quiz.dart
@@ -1,25 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
-import 'package:smart_broccoli/src/data.dart';
-import 'package:smart_broccoli/src/models.dart';
-import 'package:smart_broccoli/src/ui/shared/quiz_container.dart';
 import 'package:smart_broccoli/src/ui/shared/page.dart';
-import 'quiz_pin_box.dart';
-import 'package:weekday_selector/weekday_selector.dart';
+import 'package:flutter_settings/widgets/SettingsSection.dart';
+import 'package:flutter_settings/widgets/SettingsIcon.dart';
+import 'package:toast/toast.dart';
+import 'package:flutter_settings/util/SettingsConstants.dart';
+import 'package:flutter_settings/widgets/SettingsSelectionList.dart';
+import 'package:flutter_settings/models/settings_list_item.dart';
+import 'package:flutter_settings/widgets/SettingsCheckBox.dart';
+import 'package:flutter_settings/widgets/SettingsInputField.dart';
+import 'package:flutter_settings/widgets/SettingsSlider.dart';
 
 /// Smart quiz page
 class SmartQuiz extends StatefulWidget {
+  SmartQuiz({Key key, this.title}) : super(key: key);
+
+  final String title;
+
   @override
-  State<StatefulWidget> createState() => new _SmartQuizState();
+  _SmartQuizState createState() => new _SmartQuizState();
 }
 
 class _SmartQuizState extends State<SmartQuiz> {
-  final values = List.filled(7, true);
+  WidgetDirection direction;
+  var turnOffList;
+  var numOfNotification;
+  var wifiAtWork;
+  var _caption = "After 15 seconds of inactivity";
+  var _defaultNumOfNotification = "Unlimited";
+  var _defaultWifiAtWork;
+  var _selectionIndex = 0;
+  var _workCaption = "Not set";
+  var _wifiName;
 
   @override
-  void initState() {
-    super.initState();
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    turnOffList = new List<SettingsSelectionItem<int>>();
+    turnOffList.add(SettingsSelectionItem<int>(0, "10 " + "minutes"));
+    turnOffList.add(SettingsSelectionItem<int>(1, "30 " + "minutes"));
+    turnOffList.add(SettingsSelectionItem<int>(2, "1 " + "hour"));
+    turnOffList.add(SettingsSelectionItem<int>(3, "2 " + "hours"));
+    turnOffList.add(SettingsSelectionItem<int>(4, "4 " + "hours"));
+    turnOffList.add(SettingsSelectionItem<int>(5, "8 " + "hours"));
+    _caption = "10 minutes";
+
+    numOfNotification = new List<SettingsSelectionItem<int>>();
+    numOfNotification.add(SettingsSelectionItem<int>(0, "Unlimited"));
+    numOfNotification
+        .add(SettingsSelectionItem<int>(1, "20 notifications per day"));
+    numOfNotification
+        .add(SettingsSelectionItem<int>(2, "10 notifications per day"));
+    numOfNotification
+        .add(SettingsSelectionItem<int>(3, "5 notifications per day"));
+    numOfNotification
+        .add(SettingsSelectionItem<int>(4, "1 notifications per day"));
+    numOfNotification.add(SettingsSelectionItem<int>(5, "Never"));
+    _defaultNumOfNotification = "Unlimited";
+
+    wifiAtWork = new List<SettingsSelectionItem<int>>();
+    wifiAtWork.add(SettingsSelectionItem<int>(0, "wifi 1"));
+    wifiAtWork.add(SettingsSelectionItem<int>(1, "wifi 2"));
+    wifiAtWork.add(SettingsSelectionItem<int>(2, "wifi 3"));
+    wifiAtWork.add(SettingsSelectionItem<int>(3, "Never"));
+    _defaultWifiAtWork = "Not set";
   }
 
   @override
@@ -27,135 +71,185 @@ class _SmartQuizState extends State<SmartQuiz> {
     return CustomPage(
       title: "Smart Live Quiz",
       hasDrawer: true,
-      secondaryBackgroundColour: true,
-      child: ListView(children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(16, 16, 0, 5),
-          child: Text("General Setting",
-              style: TextStyle(color: Colors.white), textAlign: TextAlign.left),
+      child: ListView(children: <Widget>[
+        SettingsSection(
+          title: Text('General setting'),
+          settingsChildren: [
+            SettingsSelectionList<int>(
+              items: turnOffList,
+              chosenItemIndex: _selectionIndex,
+              title: 'Minimum window between notifications',
+              titleStyle: TextStyle(fontSize: 16),
+              dismissTitle: 'Cancel',
+              caption: _caption,
+              icon: new SettingsIcon(
+                icon: Icons.timer_off,
+                color: Colors.blue,
+              ),
+              onSelect: (value, index) {
+                Toast.show(
+                    "You have selected " + value.text.toString(), context);
+                setState(() {
+                  _selectionIndex = index;
+                  _caption = value.text;
+                });
+              },
+              context: context,
+            ),
+            SettingsSelectionList<int>(
+              items: numOfNotification,
+              chosenItemIndex: _selectionIndex,
+              title: 'Max number of notifications per day',
+              titleStyle: TextStyle(fontSize: 16),
+              dismissTitle: 'Cancel',
+              caption: _defaultNumOfNotification,
+              icon: new SettingsIcon(
+                icon: Icons.add_alert,
+                color: Colors.green,
+              ),
+              onSelect: (value, index) {
+                Toast.show(
+                    "You have selected " + value.text.toString(), context);
+                setState(() {
+                  _selectionIndex = index;
+                  _defaultNumOfNotification = value.text;
+                });
+              },
+              context: context,
+            ),
+          ],
         ),
-        minWin(),
-        Divider(height: 0.4, color: Colors.grey),
-        maxNotifications(),
-        Divider(height: 0.2, color: Colors.grey),
-        weekday(),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(16, 16, 0, 5),
-          child: Text("Days of week",
-              style: TextStyle(color: Colors.white), textAlign: TextAlign.left),
-        ),
-        Container(height: 150, child: picker())
+        Divider(height: 10, color: Colors.white),
+        SettingsSection(
+            title: Text(
+              'When there my calendar is not free',
+            ),
+            settingsChildren: [
+              SettingsCheckBox(
+                title: 'Allow notifications for live quiz',
+                titleStyle: TextStyle(fontSize: 16),
+                icon: new SettingsIcon(
+                  icon: Icons.live_tv,
+                  color: Colors.blueAccent,
+                ),
+                onPressed: (bool value) {
+                  Toast.show(
+                      "Allow notifications for live quiz is " +
+                          value.toString(),
+                      context);
+                },
+                value: true,
+                type: CheckBoxWidgetType.Switch,
+              ),
+              SettingsCheckBox(
+                title: 'Allow notifications for smart quiz',
+                titleStyle: TextStyle(fontSize: 16),
+                icon: new SettingsIcon(
+                  icon: Icons.lightbulb_outline,
+                  color: Colors.orange,
+                ),
+                onPressed: (bool value) {
+                  Toast.show(
+                      "Allow notifications for smart quiz is " +
+                          value.toString(),
+                      context);
+                },
+                value: false,
+                type: CheckBoxWidgetType.Switch,
+              ),
+            ]),
+        Divider(height: 10, color: Colors.white),
+        SettingsSection(
+            title: Text(
+              'Self-paced quiz setting',
+            ),
+            settingsChildren: [
+              SettingsCheckBox(
+                title: 'Send notification when commuting',
+                titleStyle: TextStyle(fontSize: 16),
+                icon: new SettingsIcon(
+                  icon: Icons.directions_walk,
+                  color: Colors.red,
+                ),
+                onPressed: (bool value) {
+                  Toast.show(
+                      "Allow notifications on commuting is " + value.toString(),
+                      context);
+                },
+                value: true,
+                type: CheckBoxWidgetType.Switch,
+              ),
+            ]),
+        Divider(height: 10, color: Colors.white),
+        SettingsSection(
+            title: Text(
+              'When at work place',
+            ),
+            settingsChildren: [
+              SettingsSelectionList<int>(
+                items: wifiAtWork,
+                chosenItemIndex: _selectionIndex,
+                title: 'Wifi at work place',
+                titleStyle: TextStyle(fontSize: 16),
+                dismissTitle: 'Cancel',
+                caption: _caption,
+                icon: new SettingsIcon(
+                  icon: Icons.wifi,
+                  color: Colors.green,
+                ),
+                onSelect: (value, index) {
+                  Toast.show(
+                      "You have selected " + value.text.toString(), context);
+                  setState(() {
+                    _selectionIndex = index;
+                    _caption = value.text;
+                  });
+                },
+                context: context,
+              ),
+              SettingsInputField(
+                dialogButtonText: 'Done',
+                title: ('Edit Working address'),
+                // titleStyle: TextStyle(fontSize: 16),
+                icon: new SettingsIcon(
+                  icon: Icons.location_on_outlined,
+                  color: Colors.blue,
+                ),
+                caption: _workCaption,
+                onPressed: (value) {
+                  if (value.toString().isNotEmpty) {
+                    Toast.show("You have Entered " + value, context);
+                    setState(() {
+                      _workCaption = value;
+                    });
+                  }
+                },
+                context: context,
+              ),
+              SettingsSlider(
+                value: 0.5,
+                activeColor: Colors.blue,
+                icon: new SettingsIcon(
+                  icon: Icons.track_changes,
+                  color: Colors.red,
+                  text: 'Radius',
+                ),
+                onChange: (value) {
+                  Toast.show(
+                      "Radius centered from work place: " +
+                          (value * 10).round().toString() +
+                          "km",
+                      context);
+                },
+              ),
+            ]),
+        Divider(height: 10, color: Colors.white),
+        SettingsSection(
+          title: Text(
+            'Days of week',
+          ),
+        )
       ]),
     );
   }
-
-  Widget minWin() => new Container(
-        padding: const EdgeInsets.all(0),
-        color: Colors.white,
-        child: Row(children: [
-          Container(
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.centerLeft,
-            width: 300,
-            child: new Text(
-              "Minimum window between notifications",
-              style: new TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 0),
-            width: 60,
-            child: Text(
-              'number',
-              style: TextStyle(
-                color: Colors.black54,
-                fontSize: 16.0,
-              ),
-              textDirection: TextDirection.rtl,
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(0),
-            width: 30,
-            alignment: Alignment.centerRight,
-            child: Icon(
-              Icons.arrow_forward_ios,
-              color: Colors.grey,
-              size: 20,
-            ),
-          )
-        ]),
-      );
-
-  Widget maxNotifications() => new Container(
-        padding: const EdgeInsets.all(0),
-        color: Colors.white,
-        child: Row(children: [
-          Container(
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.centerLeft,
-            width: 300,
-            child: new Text(
-              "Max number of notifications per day",
-              style: new TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 0),
-            width: 60,
-            child: Text(
-              'number',
-              style: TextStyle(
-                color: Colors.black54,
-                fontSize: 16.0,
-              ),
-              textDirection: TextDirection.rtl,
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(0),
-            width: 30,
-            alignment: Alignment.centerRight,
-            child: Icon(
-              Icons.arrow_forward_ios,
-              color: Colors.grey,
-              size: 20,
-            ),
-          )
-        ]),
-      );
-
-  Widget picker() => CupertinoPicker(
-          itemExtent: 30,
-          // diameterRatio: 50.0,
-          useMagnifier: true,
-          magnification: 1.1,
-          backgroundColor: Colors.white,
-          onSelectedItemChanged: (int index) {
-            print(index);
-          },
-          children: <Widget>[
-            Text(
-              "5 mins",
-              textAlign: TextAlign.justify,
-            ),
-            Text("10 mins"),
-            Text("30 mins"),
-            Text("1 h"),
-            Text("2 h"),
-            Text("3 h"),
-            Text("1 day"),
-          ]);
-
-  Widget weekday() => WeekdaySelector(
-        selectedColor: Colors.black,
-        selectedFillColor: Colors.black,
-        onChanged: (int day) {
-          setState(() {
-            final index = day % 7;
-            values[index] = !values[index];
-          });
-        },
-        values: values,
-      );
 }
