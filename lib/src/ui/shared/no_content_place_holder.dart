f3a71ea3e5ce2b59a6e74451e705a076e1fc7d6c
ekurmakaevMac <ekurmakaev@student.unimelb.edu.au>
2020-11-07 11:14:50 +1100

    all done

---
+++ lib/src/ui/shared/no_content_place_holder.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:smart_broccoli/src/models.dart';
+import 'package:smart_broccoli/src/ui/shared/quiz_container.dart';
+import 'package:smart_broccoli/src/ui/groups/group_list.dart';
+
+
+class NoContentPlaceholder extends StatelessWidget {
+  const NoContentPlaceholder({
+    Key key,
+    this.parentWidget,
+    this.text
+  }) : super(key: key);
+
+  final Object parentWidget;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+        padding: EdgeInsets.all(10.0),
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: MediaQuery.of(context).size.width * 0.8,
+      child: DecoratedBox(
+        decoration: new BoxDecoration(
+            color: Color(0xFF125E12),
+            borderRadius: BorderRadius.circular(16)),
+        child: Column(
+          children: [
+              Padding(
+              padding: const EdgeInsets.only(top: 40, bottom: 20, right: 10, left: 10),
+              child: Text(
+                "🤔",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 100,
+                ),
+              ),
+      ),
+
+
+      Padding(
+        padding: const EdgeInsets.only(bottom: 10, right: 20, left: 20),
+        child:Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+          ),
+        ) ),
+            ],),
+    )
+    );
+  }
+
+}
