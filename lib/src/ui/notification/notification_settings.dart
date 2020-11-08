396fa487d3d5b11e5783903e710629de74c72a5f
Steven Tang <steven@steventang.net>
2020-11-09 10:17:45 +1100

    wip: notification settings state

--- lib/src/ui/notification/notification_settings.dart
+++ lib/src/ui/notification/notification_settings.dart
@@ -8,11 +8,14 @@ import 'package:flutter_settings/models/settings_list_item.dart';
 import 'package:flutter_settings/widgets/SettingsInputField.dart';
 import 'package:flutter_settings/widgets/SettingsSlider.dart';
 import 'package:flutter_settings/widgets/SettingsNavigatorButton.dart';
+import 'package:provider/provider.dart';
+import 'package:smart_broccoli/src/data/prefs.dart';
+import 'package:smart_broccoli/src/models.dart';
+import 'package:smart_broccoli/src/ui/shared/indicators.dart';
 import 'package:toast/toast.dart';
 import 'package:selection_picker/selectionpicker.dart';
 import 'package:selection_picker/selection_item.dart' as day;
 
-import 'package:smart_broccoli/src/store/remote/location_api.dart';
 import 'package:smart_broccoli/src/ui/shared/page.dart';
 
 /// Smart quiz page
@@ -24,7 +27,7 @@ class NotificationSetting extends StatefulWidget {
 }
 
 class _NotificationSettingState extends State<NotificationSetting> {
-  final List<SettingsSelectionItem<int>> _minWindowList = [
+  static final List<SettingsSelectionItem<int>> _minWindowList = [
     SettingsSelectionItem<int>(0, "Unlimited"),
     SettingsSelectionItem<int>(10, "10 minutes"),
     SettingsSelectionItem<int>(30, "30 minutes"),
@@ -33,7 +36,7 @@ class _NotificationSettingState extends State<NotificationSetting> {
     SettingsSelectionItem<int>(240, "4 hours"),
     SettingsSelectionItem<int>(480, "8 hours"),
   ];
-  final List<SettingsSelectionItem<int>> _maxNumberList = [
+  static final List<SettingsSelectionItem<int>> _maxNumberList = [
     SettingsSelectionItem<int>(0, "Unlimited"),
     SettingsSelectionItem<int>(1, "1 notification per day"),
     SettingsSelectionItem<int>(5, "5 notifications per day"),
@@ -41,16 +44,7 @@ class _NotificationSettingState extends State<NotificationSetting> {
     SettingsSelectionItem<int>(10, "10 notifications per day"),
   ];
 
-  int _minWindowIndex = 0;
-  int _maxNumberIndex = 0;
-  bool _liveQuizCalendar = true;
-  bool _smartQuizCalendar = false;
-  bool _smartDetection = true;
-  String _wifiCaption;
-  LocationData _workCaption;
   double _radius = 0.5;
-  bool _onMove = true;
-  bool _commuting = true;
 
   // isSelect is used to determine whether day is selected
   // on tap, isSelected is mutated
@@ -64,203 +58,231 @@ class _NotificationSettingState extends State<NotificationSetting> {
     day.SelectionItem(name: "SU", isSelected: false, identifier: 6)
   ];
 
+  // Copy of notification preferences
+  NotificationPrefs _copy;
+
   @override
   Widget build(BuildContext context) {
     return CustomPage(
       title: "Notification setting",
       hasDrawer: true,
-      child: Container(
-        padding: EdgeInsets.only(top: 8),
-        child: ListView(children: <Widget>[
-          SettingsSection(
-            title: Text('General settings'),
-            settingsChildren: [
-              /// Minimum windows
-              SettingsSelectionList<int>(
-                items: _minWindowList,
-                //default selected item index, it will be the first item by default.
-                chosenItemIndex: _minWindowIndex,
-                title: 'Minimum window between notifications',
-                titleStyle: TextStyle(fontSize: 16),
-                dismissTitle: 'Cancel',
-                caption: _minWindowList[_minWindowIndex].text,
-                icon: new SettingsIcon(
-                  icon: Icons.timer_off,
-                  color: Colors.blue,
+      child: FutureBuilder(
+        future: Provider.of<UserProfileModel>(context).getNotificationPrefs(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return LoadingIndicator(EdgeInsets.all(16));
+          if (snapshot.hasError)
+            return Center(child: Text("Cannot load settings"));
+
+          // Make a copy
+          if (_copy == null) _copy = snapshot.data;
+
+          return Container(
+            padding: EdgeInsets.only(top: 8),
+            child: ListView(children: <Widget>[
+              SettingsSection(
+                title: Text('General settings'),
+                settingsChildren: [
+                  // Minimum windows
+                  SettingsSelectionList<int>(
+                    items: _minWindowList,
+                    chosenItemIndex: _map_1(_minWindowList.indexWhere(
+                        (element) => element.value == _copy.minWindow)),
+                    title: 'Minimum window between notifications',
+                    titleStyle: TextStyle(fontSize: 16),
+                    dismissTitle: 'Cancel',
+                    caption: _minWindowList[_map_1(_minWindowList.indexWhere(
+                            (element) => element.value == _copy.minWindow))]
+                        .text,
+                    icon: new SettingsIcon(
+                        icon: Icons.timer_off, color: Colors.blue),
+                    onSelect: (value, index) {
+                      setState(() => _copy.minWindow = value);
+                    },
+                    context: context,
+                  ),
+
+                  // Max number of notifications
+                  SettingsSelectionList<int>(
+                    items: _maxNumberList,
+                    chosenItemIndex: _map_1(_maxNumberList.indexWhere(
+                        (element) => element.value == _copy.maxPerDay)),
+                    title: 'Max number of notifications per day',
+                    titleStyle: TextStyle(fontSize: 16),
+                    dismissTitle: 'Cancel',
+                    caption: _maxNumberList[_map_1(_maxNumberList.indexWhere(
+                            (element) => element.value == _copy.maxPerDay))]
+                        .text,
+                    icon: new SettingsIcon(
+                      icon: Icons.add_alert,
+                      color: Colors.green,
+                    ),
+                    onSelect: (value, index) {
+                      setState(() => _copy.maxPerDay = value);
+                    },
+                    context: context,
+                  ),
+                ],
+              ),
+              Divider(height: 4, color: Colors.white),
+
+              // TODO
+              // Days of week
+              SettingsSection(title: Text('Days of week')),
+              Container(
+                height: 90,
+                child: SelectionPicker(
+                  items: days,
+                  showSelectAll: false,
+                  showTitle: false,
+                  textColor: Colors.black54,
+                  backgroundColorNoSelected: Colors.grey[200],
+                  backgroundColorSelected: Color(0xFFFEC12D),
+                  onSelected: (List<day.SelectionItem> items) {
+                    print(days.map((e) => e.isSelected));
+                  },
+                  aligment: Alignment.center,
                 ),
-                onSelect: (value, index) {
-                  setState(() => _minWindowIndex = index);
-                },
-                context: context,
+              ),
+              Divider(height: 4, color: Colors.white),
+
+              // Calendar
+              SettingsSection(
+                title: Text('When my calendar is not free'),
               ),
 
-              /// Max number of notifications
-              SettingsSelectionList<int>(
-                items: _maxNumberList,
-                //default selected item index, it will be the first item by default.
-                chosenItemIndex: _maxNumberIndex,
-                title: 'Max number of notifications per day',
+              // Calendar live quiz
+              _switchListTile(
+                  icon: Icon(Icons.live_tv, color: Colors.blueAccent),
+                  title: "Allow notifications for live quiz",
+                  onTap: () {
+                    setState(() =>
+                        _copy.allowLiveIfCalendar = !_copy.allowLiveIfCalendar);
+                  },
+                  value: _copy.allowLiveIfCalendar,
+                  disabled: false),
+              Separator(),
+
+              // Calendar smart quiz
+              _switchListTile(
+                  icon: Icon(Icons.lightbulb_outline, color: Colors.orange),
+                  title: "Allow notifications for smart quiz",
+                  onTap: () {
+                    setState(() => _copy.allowSelfPacedIfCalendar =
+                        !_copy.allowSelfPacedIfCalendar);
+                  },
+                  value: _copy.allowSelfPacedIfCalendar,
+                  disabled: false),
+              Divider(height: 4, color: Colors.white),
+
+              // Work setting
+              SettingsSection(
+                title: Text('Don\'t notify me at work'),
+              ),
+
+              // Smart detection
+              _switchListTile(
+                  icon:
+                      Icon(Icons.auto_awesome_mosaic, color: Colors.blueAccent),
+                  title: "Smart detection",
+                  onTap: () {
+                    setState(() => _copy.workSmart = !_copy.workSmart);
+                  },
+                  value: _copy.workSmart,
+                  disabled: false),
+
+              // Wifi setting
+              SettingsInputField(
                 titleStyle: TextStyle(fontSize: 16),
-                dismissTitle: 'Cancel',
-                caption: _maxNumberList[_maxNumberIndex].text,
+                dialogButtonText: 'Done',
+                title: ('Wifi at work place'),
                 icon: new SettingsIcon(
-                  icon: Icons.add_alert,
+                  icon: Icons.wifi,
                   color: Colors.green,
                 ),
-                onSelect: (value, index) {
-                  setState(() => _maxNumberIndex = index);
+                caption: _copy.workSSID == null ? "Not set" : _copy.workSSID,
+                onPressed: (value) {
+                  if (value != null && value.isNotEmpty) {
+                    setState(() => _copy.workSSID = value);
+                  }
                 },
                 context: context,
               ),
-            ],
-          ),
-          Divider(height: 4, color: Colors.white),
 
-          ///Days of week
-          SettingsSection(title: Text('Days of week')),
-          Container(
-            height: 90,
-            child: SelectionPicker(
-              items: days,
-              showSelectAll: false,
-              showTitle: false,
-              textColor: Colors.black54,
-              backgroundColorNoSelected: Colors.grey[200],
-              backgroundColorSelected: Color(0xFFFEC12D),
-              onSelected: (List<day.SelectionItem> items) {
-                print(days.map((e) => e.isSelected));
-              },
-              aligment: Alignment.center,
-            ),
-          ),
-          Divider(height: 4, color: Colors.white),
+              // Address setting
+              SettingsNavigatorButton(
+                  title: 'Work address',
+                  titleStyle: TextStyle(fontSize: 16),
+                  icon: new SettingsIcon(
+                    icon: Icons.location_on_outlined,
+                    color: Colors.orange,
+                  ),
+                  context: context,
+                  caption: _copy.workLocation == null
+                      ? "Not set"
+                      : _copy.workLocation,
+                  onPressed: () async {
+                    var location =
+                        await Navigator.of(context).pushNamed("/work_address");
+                    if (location != null)
+                      setState(() => _copy.workLocation = location);
+                  }),
 
-          ///Calendar
-          SettingsSection(
-            title: Text('When there my calendar is not free'),
-          ),
-
-          ///Calendar live quiz
-          _switchListTile(
-              icon: Icon(Icons.live_tv, color: Colors.blueAccent),
-              title: "Allow notifications for live quiz",
-              onTap: () {
-                setState(() => _liveQuizCalendar = !_liveQuizCalendar);
-              },
-              value: _liveQuizCalendar,
-              disabled: false),
-          Separator(),
-
-          ///Calendar smart quiz
-          _switchListTile(
-              icon: Icon(Icons.lightbulb_outline, color: Colors.orange),
-              title: "Allow notifications for smart quiz",
-              onTap: () {
-                setState(() => _smartQuizCalendar = !_smartQuizCalendar);
-              },
-              value: _smartQuizCalendar,
-              disabled: false),
-          Divider(height: 4, color: Colors.white),
-
-          ///Work setting
-          SettingsSection(
-            title: Text('Don\'t notify me at work'),
-          ),
-
-          /// Smart detection
-          _switchListTile(
-              icon: Icon(Icons.auto_awesome_mosaic, color: Colors.blueAccent),
-              title: "Smart detection",
-              onTap: () {
-                setState(() => _smartDetection = !_smartDetection);
-              },
-              value: _smartDetection,
-              disabled: false),
-
-          /// Wifi setting
-          SettingsInputField(
-            titleStyle: TextStyle(fontSize: 16),
-            dialogButtonText: 'Done',
-            title: ('Wifi at work place'),
-            icon: new SettingsIcon(
-              icon: Icons.wifi,
-              color: Colors.green,
-            ),
-            caption: _wifiCaption == null ? "Not set" : _wifiCaption,
-            onPressed: (value) {
-              if (value != null && value.isNotEmpty) {
-                setState(() => _wifiCaption = value);
-              }
-            },
-            context: context,
-          ),
-
-          /// Address setting
-          SettingsNavigatorButton(
-              title: 'Work address',
-              titleStyle: TextStyle(fontSize: 16),
-              icon: new SettingsIcon(
-                icon: Icons.location_on_outlined,
-                color: Colors.orange,
+              // Radius setting
+              // TODO
+              SettingsSlider(
+                value: _radius,
+                activeColor: Colors.blue,
+                icon: new SettingsIcon(
+                  icon: Icons.track_changes,
+                  color: Colors.red,
+                  text: 'Radius',
+                ),
+                onChange: (value) {
+                  Toast.show(
+                      "Radius centered from work place: ${(value * 10).round().toString()} km",
+                      context);
+                  setState(() => _radius = value);
+                },
               ),
-              context: context,
-              caption: _workCaption == null ? "Not set" : _workCaption.name,
-              onPressed: () async {
-                var location =
-                    await Navigator.of(context).pushNamed("/work_address");
-                if (location != null) setState(() => _workCaption = location);
-              }),
+              Divider(height: 6, color: Colors.white),
 
-          /// Radius setting
-          SettingsSlider(
-            value: _radius,
-            activeColor: Colors.blue,
-            icon: new SettingsIcon(
-              icon: Icons.track_changes,
-              color: Colors.red,
-              text: 'Radius',
-            ),
-            onChange: (value) {
-              Toast.show(
-                  "Radius centered from work place: ${(value * 10).round().toString()} km",
-                  context);
-              setState(() => _radius = value);
-            },
-          ),
-          Divider(height: 6, color: Colors.white),
+              ///Move settings
+              SettingsSection(title: Text('When moving')),
 
-          ///Move settings
-          SettingsSection(title: Text('When moving')),
+              /// Move
+              _switchListTile(
+                  icon: Icon(Icons.directions_walk, color: Colors.amber),
+                  title: "Allow notifications on the move",
+                  onTap: () {
+                    setState(() {
+                      _copy.allowOnTheMove = !_copy.allowOnTheMove;
+                      _copy.allowOnCommute = _copy.allowOnTheMove;
+                    });
+                  },
+                  value: _copy.allowOnTheMove),
+              Separator(),
 
-          /// Move
-          _switchListTile(
-              icon: Icon(Icons.directions_walk, color: Colors.amber),
-              title: "Allow notifications on the move",
-              onTap: () {
-                setState(() {
-                  _onMove = !_onMove;
-                  _commuting = _onMove;
-                });
-              },
-              value: _onMove),
-          Separator(),
-
-          /// Commute
-          _switchListTile(
-            icon: Icon(Icons.train, color: Colors.blueAccent),
-            title: "Allow notifications on commute",
-            onTap: () {
-              setState(() {
-                _commuting = !_commuting;
-              });
-            },
-            value: _commuting,
-            disabled: !_onMove,
-          ),
-        ]),
+              /// Commute
+              _switchListTile(
+                icon: Icon(Icons.train, color: Colors.blueAccent),
+                title: "Allow notifications on commute",
+                onTap: () {
+                  setState(() {
+                    _copy.allowOnCommute = !_copy.allowOnCommute;
+                  });
+                },
+                value: _copy.allowOnCommute,
+                disabled: !_copy.allowOnTheMove,
+              ),
+            ]),
+          );
+        },
       ),
     );
   }
+
+  // Maps -1 to 0
+  int _map_1(int index) => index == -1 ? 0 : index;
 
   /// A switch list tile mimicking the ListTile provided by the package
   /// The package contained state interally, which we could not modify
